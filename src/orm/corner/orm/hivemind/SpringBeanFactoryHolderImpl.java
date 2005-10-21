e7317c655bf5729cd4288bb7edd4f29c5e65288c
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2005-10-21 07:24:11 +0000

    添加Hivemind对Spring容器的支持
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@156 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/orm/corner/orm/hivemind/SpringBeanFactoryHolderImpl.java
@@ -0,0 +1,31 @@
+package corner.orm.hivemind;
+
+import org.apache.hivemind.events.RegistryShutdownListener;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import corner.orm.spring.SpringContainer;
+/**
+ * 
+ * 把Spring包装后提供给Hivemind的服务。
+ * @author	<a href="http://wiki.java.net/bin/view/People/JunTsai">Jun Tsai</a>
+ * @version	$Revision$
+ * @since	2005-9-9
+ */
+public class SpringBeanFactoryHolderImpl extends
+		org.apache.hivemind.lib.impl.SpringBeanFactoryHolderImpl implements
+		RegistryShutdownListener {
+
+	 public BeanFactory getBeanFactory() {
+		    if (super.getBeanFactory() == null) {
+                    super.setBeanFactory(SpringContainer.getInstance().getApplicationContext());
+            }
+            
+            return super.getBeanFactory();
+    }
+
+    public void registryDidShutdown() {
+           ((ConfigurableApplicationContext) super.getBeanFactory()).close();
+    }
+
+}
