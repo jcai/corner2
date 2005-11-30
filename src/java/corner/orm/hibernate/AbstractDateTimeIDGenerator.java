c46b8005a00dd9ebb95c85f97a094d7c1e27d661
jun <jun@bda6c913-8cf9-0310-89cc-b5856a464ced>
2005-11-30 07:39:27 +0000

    纠正了不能得到无重复ID的错误
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@436 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/java/corner/orm/hibernate/AbstractDateTimeIDGenerator.java
@@ -0,0 +1,49 @@
+//==============================================================================
+//file :       $Id$
+//project:     corner
+//
+//last change:	date:       $Date$
+//           	by:         $Author$
+//           	revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	China Java Users Group http://cnjug.dev.java.net
+//License:		the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.hibernate;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 抽象的DateTimeGenerator对象.
+ * 
+ * @author	<a href="http://wiki.java.net/bin/view/People/JunTsai">Jun Tsai</a>
+ * @version	$Revision$
+ * @since	2005-11-30
+ */
+public abstract class AbstractDateTimeIDGenerator {
+	private static final Log log=LogFactory.getLog(AbstractDateTimeIDGenerator.class);
+	protected String upTime = null;
+	/* get now time formatted*/
+	protected String getNowTimeFormatted() {
+		Calendar rightNow = Calendar.getInstance();
+		DateFormat formator = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+		String currentTime = formator.format(rightNow.getTime());
+		if (currentTime.equals(upTime)) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				log.error(e.getMessage(), e);
+			}
+			currentTime = getNowTimeFormatted();
+		}
+		upTime=currentTime;
+		return currentTime;
+	}
+	
+}
