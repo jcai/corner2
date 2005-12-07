afd92499f5e634725dc6fe0045cb16b6792d6e7a
agilewang <agilewang@bda6c913-8cf9-0310-89cc-b5856a464ced>
2005-12-07 09:02:45 +0000

    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@492 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/java/corner/util/TapestryHtmlFormatter.java
+++ src/java/corner/util/TapestryHtmlFormatter.java
@@ -16,57 +16,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.CharBuffer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * 
  * 
- * @author	AgileWang
- * @version	$Revision$
- * @since	2005-12-7
+ * @author AgileWang
+ * @version $Revision$
+ * @since 2005-12-7
  */
 public class TapestryHtmlFormatter {
-//	如果有其它的tag,如select,直接加到checkbox后面就可
-	 private static final Pattern pattern = Pattern.compile(
-	   "<(form|input|textarea|checkbox|select|button|option|)([^<>]*)(name=\"?([^<>\\s\"]+)\"?)([jwcid=\"?([^<>\\s\"]+)\"?]?+)([^<>]*)>",
-	   Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
-	 
-	 public static  StringBuffer format(InputStream io) throws IOException{
-		 StringBuffer inputBuffer = new StringBuffer();
-		  
-		  BufferedReader  isr = new BufferedReader(new InputStreamReader(io));
-		  String s = null;
-		  while(( s = isr.readLine())!=null){
-		   inputBuffer.append(s); 
-		   if(s.length()>0){
-		    inputBuffer.append("\n");
-		   }
-		  }  
-		  isr.close();
-		  
-		  Matcher matcher = pattern.matcher(inputBuffer.toString());   
-		    
-		     StringBuffer buf = new StringBuffer();
-		     while ((matcher.find())) {    
-		         String s1 = matcher.group(1);
-		         String s2 = matcher.group(2);
-		         //name
-		         String s3 = matcher.group(3);
-		         String name = matcher.group(4);
-		         
-		         
-		         String s7 = matcher.group(5);
-		         
-		         String value= String.format("<%s%s%s%s%s>",s1,s2,s3," jwcid=\""+name+"Field\"",s7); 
-		          
-		         matcher.appendReplacement(buf, value);
-		     }
-		     
-		     matcher.appendTail(buf);
-		     
-		     return buf;
-		     
-	 }
+	// 如果有其它的tag,如select,直接加到checkbox后面就可
+
 	
+	private static final Pattern jwcidPattern = Pattern.compile(".*(jwcid\\s*=\\s*\"?.*\"?).*");
+	
+	private static final Pattern pattern = Pattern.compile(
+			   "<(form|input|textarea|checkbox|select|button|option|)([^<>]*)(name=\"?([^<>\\s\"]+)\"?)([^<>]*)>",
+			   Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
+			 
+
+
+	public static StringBuffer format(InputStream io) throws IOException {
+		StringBuffer inputBuffer = new StringBuffer();
+
+		BufferedReader isr = new BufferedReader(new InputStreamReader(io));
+		String s = null;
+		char[] buffer = new char[1024];
+		int len = -1;
+		while ((len = isr.read(buffer)) != -1) {
+			inputBuffer.append(buffer, 0, len);
+		}
+		isr.close();
+
+		Matcher matcher = pattern.matcher(inputBuffer.toString());
+
+		StringBuffer buf = new StringBuffer();
+		while ((matcher.find())) {
+			String ms = matcher.group();			
+			String value = null;
+			String s1 = matcher.group(1).trim();
+			String s2 = matcher.group(2).trim().replaceAll("jwcid=\"?[^\\s]*\"?","");
+			String s3 = matcher.group(3).trim();
+			String name = matcher.group(4).trim();
+			String s5 = matcher.group(5).trim().replaceAll("jwcid=\"?[^\\s]*\"?","");;
+			value = String.format("<%s%s %s %s%s>", s1, s2, s3, "jwcid=\""
+					+ name + "Field\"", s5);
+			matcher.appendReplacement(buf, value);
+		}
+
+		matcher.appendTail(buf);
+		return buf;
+	}
+
 }
