e20a331e09244e3b8a8aedf0d4556688fbf1127e
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-05-11 05:54:58 +0000

    加入hibernate datasequeer
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@756 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/java/corner/orm/hibernate/IPersistent.java
@@ -0,0 +1,10 @@
+package corner.orm.hibernate;
+
+/**
+ * 所有的实体均实现该接口.主要用来统一定义和序列化.
+ * @author jun
+ * @see corner.orm.tapestry.HibernateAdapter
+ */
+public interface IPersistent {
+
+}
---
+++ src/java/corner/orm/tapestry/RawURLLinkRenderer.java
@@ -0,0 +1,21 @@
+package corner.orm.tapestry;
+
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.components.ILinkComponent;
+import org.apache.tapestry.link.ILinkRenderer;
+
+/**
+ * 扩展tapestry的renderer,供JavaScript中产生URL.
+ * 
+ * @author Jun Tsai
+ * @version $Revison$
+ * @since 2006-4-18
+ */
+public class RawURLLinkRenderer implements ILinkRenderer {
+	
+	public void renderLink(IMarkupWriter writer, IRequestCycle cycle,
+			ILinkComponent linkComponent) {
+		writer.print(linkComponent.getLink(cycle).getAbsoluteURL(), true);
+	}
+}
