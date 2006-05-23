37fd202a3cc569ace4b7c5b95bd4544ed40159cf
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-05-23 09:44:47 +0000

    加入persistent object的form和list
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@812 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/java/corner/orm/tapestry/page/PoFormPage.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package corner.orm.tapestry.page;
+
+import corner.orm.hibernate.IPersistent;
+
+/**
+ * 对实体的表单操作。
+ * @author Jun Tsai
+ * @version $Revison$
+ * @since 2006-5-23
+ */
+public abstract class PoFormPage extends EntityFormPage<IPersistent> {
+
+}
---
+++ src/java/corner/orm/tapestry/page/PoListPage.java
@@ -0,0 +1,13 @@
+package corner.orm.tapestry.page;
+
+import corner.orm.hibernate.IPersistent;
+
+/**
+ * 抽象的对所有实体的操作。
+ * @author Jun Tsai
+ * @version $Revison$
+ * @since 2006-5-23
+ */
+public abstract class PoListPage extends EntityListPage<IPersistent> {
+
+}
