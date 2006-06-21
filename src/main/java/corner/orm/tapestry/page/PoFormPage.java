2e64814090b489b10cd09a013131583f1abe447f
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-06-21 06:36:44 +0000

    提供了表单操作的类
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1199 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/page/PoFormPage.java
+++ src/main/java/corner/orm/tapestry/page/PoFormPage.java
@@ -10,6 +10,6 @@ package corner.orm.tapestry.page;
  * @version $Revision$
  * @since 2006-5-23
  */
-public abstract class PoFormPage extends EntityFormPage<Object> {
+public abstract class PoFormPage extends AbstractEntityFormPage<Object> {
 
 }
