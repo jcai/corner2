d336a51e9332042a12876ee2771c08b5fcf68874
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-06-22 07:20:08 +0000

    纠正Collection为空的时候的错误
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1224 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/table/RelativePersistentBasicTableModel.java
+++ src/main/java/corner/orm/tapestry/table/RelativePersistentBasicTableModel.java
@@ -5,6 +5,7 @@ package corner.orm.tapestry.table;
 
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 
 import org.apache.tapestry.contrib.table.model.IBasicTableModel;
@@ -18,77 +19,101 @@ import corner.orm.hibernate.v3.HibernateObjectRelativeUtils;
 import corner.service.EntityService;
 import corner.util.BeanUtils;
 
-
 /**
  * 提供显示本对象关联对象的集合。
+ * 
  * @author Jun Tsai
  * @version $Revision$
  * @since 2.0.4
  */
-public class RelativePersistentBasicTableModel<T> implements IBasicTableModel{
+public class RelativePersistentBasicTableModel<T> implements IBasicTableModel {
 	/**
 	 * 关联属性名称。
 	 */
 	private String relativeProName;
+
 	/**
 	 * 基对象。
 	 */
 	private T rootedObj;
-	private int rows=-1;
+
+	private int rows = -1;
+
 	private EntityService entityService;
 
-	public RelativePersistentBasicTableModel(EntityService entityService,T rootedObj,String relativeProName){
-		this.rootedObj=rootedObj;
-		this.relativeProName=relativeProName;
-		this.entityService=entityService;
+	public RelativePersistentBasicTableModel(EntityService entityService,
+			T rootedObj, String relativeProName) {
+		this.rootedObj = rootedObj;
+		this.relativeProName = relativeProName;
+		this.entityService = entityService;
 	}
 
-	private Collection getRelativeCollection(){
-		return (Collection) BeanUtils.getProperty(rootedObj,relativeProName);
+	private Collection getRelativeCollection() {
+		return (Collection) BeanUtils.getProperty(rootedObj, relativeProName);
 	}
+
 	/**
-	 *
+	 * 
 	 * @see org.apache.tapestry.contrib.table.model.IBasicTableModel#getRowCount()
 	 */
 	public int getRowCount() {
-		if (rows == -1) {
-			rows=((Integer) ((HibernateObjectRelativeUtils) this.entityService
-					.getObjectRelativeUtils()).getHibernateTemplate()
-					.execute(new HibernateCallback(){
 
-						public Object doInHibernate(Session session) throws HibernateException, SQLException {
-							Query query=session.createFilter(getRelativeCollection(),"select count(*)");
+		if (rows == -1) {
+			final Collection c = this.getRelativeCollection();
+			if (c == null) {
+				return 0;
+			}
+			rows = ((Integer) ((HibernateObjectRelativeUtils) this.entityService
+					.getObjectRelativeUtils()).getHibernateTemplate().execute(
+					new HibernateCallback() {
+
+						public Object doInHibernate(Session session)
+								throws HibernateException, SQLException {
+							Query query = session.createFilter(c,
+									"select count(*)");
 							return query.iterate().next();
-						}})).intValue();
+						}
+					})).intValue();
 
 		}
 		return rows;
 
 	}
+
 	/**
-	 *
-	 * @see org.apache.tapestry.contrib.table.model.IBasicTableModel#getCurrentPageRows(int, int, org.apache.tapestry.contrib.table.model.ITableColumn, boolean)
+	 * 
+	 * @see org.apache.tapestry.contrib.table.model.IBasicTableModel#getCurrentPageRows(int,
+	 *      int, org.apache.tapestry.contrib.table.model.ITableColumn, boolean)
 	 */
 	public Iterator getCurrentPageRows(final int nFirst, final int nPageSize,
 			final ITableColumn column, final boolean sort) {
-		return((Iterator) ((HibernateObjectRelativeUtils) this.entityService
-				.getObjectRelativeUtils()).getHibernateTemplate()
-				.execute(new HibernateCallback(){
+		final Collection c = this.getRelativeCollection();
+		if (c == null) {
+			return Collections.EMPTY_LIST.iterator();
+		}
 
-					public Object doInHibernate(Session session) throws HibernateException, SQLException {
-						String orderStr="";
+		return ((Iterator) ((HibernateObjectRelativeUtils) this.entityService
+				.getObjectRelativeUtils()).getHibernateTemplate().execute(
+				new HibernateCallback() {
+
+					public Object doInHibernate(Session session)
+							throws HibernateException, SQLException {
+						String orderStr = "";
 
 						if (column != null) {
 
-							orderStr="order by "+column.getColumnName()+(sort ? " " :" desc");
+							orderStr = "order by " + column.getColumnName()
+									+ (sort ? " " : " desc");
 						}
-						Query query=session.createFilter(getRelativeCollection(),orderStr);
+						Query query = session.createFilter(
+								getRelativeCollection(), orderStr);
 
 						query.setFirstResult(nFirst);
 						query.setMaxResults(nPageSize);
 
 						return query.iterate();
-					}}));
+					}
+				}));
 	}
 
 }
