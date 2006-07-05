d7864f2d2e432666d7756709463f329bf3991762
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-07-05 02:15:56 +0000

    增加Vector UserType对象
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1286 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/demo/model/one/A.java
+++ src/main/java/corner/demo/model/one/A.java
@@ -1,5 +1,7 @@
 package corner.demo.model.one;
 
+import java.util.Vector;
+
 import corner.demo.model.AbstractModel;
 
 /**
@@ -11,11 +13,30 @@ import corner.demo.model.AbstractModel;
  * @hibernate.cache usage="read-write"
  * @hibernate.mapping auto-import="false"
  */
-public class A extends AbstractModel{
+public class A extends AbstractModel {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -1296703401706863951L;
 
+	/**
+	 * @hibernate.property type="corner.orm.hibernate.v3.VectorType"
+	 */
+	private Vector<String> colors;
+
+	/**
+	 * @return Returns the colors.
+	 */
+	public Vector<String> getColors() {
+		return colors;
+	}
+
+	/**
+	 * @param colors
+	 *            The colors to set.
+	 */
+	public void setColors(Vector<String> colors) {
+		this.colors = colors;
+	}
 }
