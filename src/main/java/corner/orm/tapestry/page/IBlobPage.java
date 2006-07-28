f6d177f7a829baa173a0295e5d9d12506f97529d
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-07-28 03:16:15 +0000

    从poison中重构出来的IBlobPage
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1419 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/tapestry/page/IBlobPage.java
@@ -0,0 +1,41 @@
+//==============================================================================
+//file :        IBlobPage.java
+//project:      poison-system
+//
+//last change:	date:       $Date$
+//           	by:         $Author$
+//           	revision:   $Revision$
+//------------------------------------------------------------------------------
+//==============================================================================
+
+package corner.orm.tapestry.page;
+
+import org.apache.tapestry.request.IUploadFile;
+
+/**
+ * 抽象的针对blob的页面. 
+ * <p>提供了上传文件的句柄,可编辑blob对象的判断,显示图像的Asset. 
+ * @author	<a href="http://wiki.java.net/bin/view/People/JunTsai">Jun Tsai</a>
+ * @version	$Revision$
+ * @since	2006-2-6
+ */
+public interface IBlobPage {
+	
+	/**
+	 * 上传文件的句柄.
+	 * @return 上传文件句柄.
+	 */
+	public abstract IUploadFile getUploadFile();
+	/**
+	 * 是否编辑blob.
+	 * @return 是否编辑.
+	 */
+	public abstract boolean isEditBlob();
+	/**
+	 * 设定是否编辑blob对象.
+	 * @param isEditBlob 是否编辑.
+	 */
+	public abstract void setEditBlob(boolean isEditBlob);
+	
+
+}
