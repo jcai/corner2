ab779056844f155cd83befd148b573750b71e769
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-08-21 01:32:36 +0000

    更新注释
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1510 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/page/relative/IRelativeObjectOperatorSupport.java
+++ src/main/java/corner/orm/tapestry/page/relative/IRelativeObjectOperatorSupport.java
@@ -17,11 +17,16 @@ import org.apache.tapestry.annotations.InjectObject;
 import corner.orm.tapestry.page.relative.support.IRelativeObjectOperator;
 
 /**
+ * 提供了对关联对象操作.
  * @author jcai
  * @version $Revision$
  * @since 2.1
  */
 public interface IRelativeObjectOperatorSupport {
+	/**
+	 * 得到关联对象操作者.
+	 * @return 关联对象操作者。
+	 */
 	@InjectObject("service:corner.orm.relativeOperator")
 	public abstract IRelativeObjectOperator getRelativeObjectOperator();
 }
