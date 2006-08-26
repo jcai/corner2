fd221af492b49deef2fc3af9ddb804fd85b0bb00
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-08-26 03:33:41 +0000

    增加属性名字可以在.page中声明
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1540 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/page/relative/ReflectRelativeEntityFormPage.java
+++ src/main/java/corner/orm/tapestry/page/relative/ReflectRelativeEntityFormPage.java
@@ -16,12 +16,22 @@ import corner.util.EntityConverter;
  */
 public abstract class ReflectRelativeEntityFormPage extends
 		AbstractRelativeEntityFormPage<Object,Object> {
+	
+	/**
+	 * 得到关联关系的属性名字，此名字可以在.page文件中设置.可以避免多余的Java文件.
+	 * @return 关联的属性名字.
+	 */
+	public abstract String getRelativeProName();
 	/**
 	 * 得到关联的对象的属性名称。
-	 * @return
+	 * <P>提供子类覆写该方法。
+	 * @return 属性名字。
 	 */
 	protected  String getRelativePropertyName(){
-		return EntityConverter.getClassNameAsPropertyName(this.getRootedObject());
+		if(this.getRelativeProName()==null)
+			return EntityConverter.getClassNameAsPropertyName(this.getRootedObject());
+		else
+			return this.getRelativeProName();
 	}
 
 	/**
