85bec9db902e5fc5906ba4fa7648348799d1930e
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-08-31 06:39:38 +0000

    增加了TextAreaBox组件使用的js,并增加了静态测试用例
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1582 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/js/corner/widget/TextAreaBox.js
@@ -0,0 +1,118 @@
+/*
+	Copyright (c) Beijing Maxinfo Technology Co.,Ltd.
+	All Rights Reserved.
+
+*/
+
+dojo.provide("corner.widget.TextAreaBox");
+
+dojo.require("dojo.widget.html.ComboBox");
+dojo.require("dojo.widget.*");
+dojo.require("dojo.widget.html.stabile");
+
+dojo.widget.tags.addParseTreeHandler("dojo:TextAreaBox");
+
+dojo.widget.defineWidget(
+	"corner.widget.TextAreaBox",
+	dojo.widget.html.ComboBox,
+	{
+		widgetType: "TextAreaBox",
+		forceValidOption: false,
+		comboBoxValue: null,
+		
+		setValue: function(value) {
+			this.comboBoxValue.value = value;
+		},
+
+		setLabel: function(value){
+			this.comboBoxSelectionValue.value = value;
+			if (this.textInputNode.value != value) {
+				this.textInputNode.value = value;
+			}
+		},	  
+		
+		getLabel: function(){
+			return this.comboBoxSelectionValue.value;
+		},
+/*
+		getState: function() {
+			return {
+				value: this.getValue(),
+				label: this.getLabel()
+			};
+		},
+*/
+		onKeyUp: function(evt){
+			this.comboBoxValue.value = this.textInputNode.value;
+			this.setValue(this.textInputNode.value);
+			this.setLabel(this.textInputNode.value);
+		},
+
+/*
+		setState: function(state) {
+			this.setValue(state.value);
+			this.setLabel(state.label);
+		},
+*/
+		setAllValues: function(value1, value2){
+			this.setValue(value1);
+			this.setLabel(value1);
+		},
+		//overwirte the default Html templete URL,use our Html templete instead
+		//指定自己的html模版文件
+		templatePath: dojo.uri.dojoUri("../corner/widget/templates/TextAreaBox.html"),
+		templateCssPath: dojo.uri.dojoUri("../corner/widget/templates/TextAreaBox.css"),
+		startSearchFromInput: function(){
+			var searchStr = this.textInputNode.value
+			if(searchStr != null && searchStr.length>0 && searchStr.search(';')>-1){
+				this.startSearch(searchStr.substr(searchStr.lastIndexOf(';')+1,searchStr.length));
+			}
+			else{
+				this.startSearch(this.textInputNode.value);
+			}
+		},
+
+		//overwirte the selectOptionMethod,record all of the user input
+		//在用户选择了下拉菜单之后，记录并现实先前输入的全部内容
+		selectOption: function(evt){
+			var tgt = null;
+			if(!evt){
+				evt = { target: this._highlighted_option };
+			}
+	
+			if(!dojo.dom.isDescendantOf(evt.target, this.optionsListNode)){
+				// handle autocompletion where the the user has hit ENTER or TAB
+	
+				// if the input is empty do nothing
+				if(!this.textInputNode.value.length){
+					return;
+				}
+				tgt = dojo.dom.firstElement(this.optionsListNode);
+	
+				// user has input value not in option list
+				if(!tgt || !this._isInputEqualToResult(tgt.getAttribute("resultName"))){
+					return;
+				}
+				// otherwise the user has accepted the autocompleted value
+			}else{
+				tgt = evt.target; 
+			}
+	
+			while((tgt.nodeType!=1)||(!tgt.getAttribute("resultName"))){
+				tgt = tgt.parentNode;
+				if(tgt === document.body){
+					return false;
+				}
+			}
+	
+			this.textInputNode.value = tgt.getAttribute("resultName");
+			this.selectedResult = [tgt.getAttribute("resultName"), tgt.getAttribute("resultValue")];
+			this.setAllValues(this.comboBoxSelectionValue.value.substr(0,this.comboBoxSelectionValue.value.lastIndexOf(';')+1)+tgt.getAttribute("resultName")+":", this.comboBoxSelectionValue.value.substr(0,this.comboBoxSelectionValue.value.lastIndexOf(';')+1));
+
+			if(!evt.noHide){
+				this.hideResultList();
+				this.setSelectedRange(this.textInputNode, 0, null);
+			}
+			this.tryFocus();
+		}
+	});
