cd9de149a8a8bcb98026c46607f9fca3f3fb5f3b
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-01 10:18:18 +0000

    去掉了无效的脚本
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1589 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/textareabox/TextAreaBox.java
+++ src/main/java/corner/orm/tapestry/component/textareabox/TextAreaBox.java
@@ -10,17 +10,24 @@
 
 package corner.orm.tapestry.component.textareabox;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.tapestry.IMarkupWriter;
 import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.PageRenderSupport;
 import org.apache.tapestry.Tapestry;
+import org.apache.tapestry.TapestryUtils;
 import org.apache.tapestry.dojo.form.Autocompleter;
 import org.apache.tapestry.dojo.form.IAutocompleteModel;
+import org.apache.tapestry.engine.DirectServiceParameter;
+import org.apache.tapestry.engine.ILink;
 import org.apache.tapestry.json.IJSONWriter;
 import org.apache.tapestry.json.JSONObject;
 import org.apache.tapestry.valid.ValidatorException;
+
+import corner.util.BeanUtils;
 
 /**
  * @author Ghost
@@ -28,6 +35,66 @@ import org.apache.tapestry.valid.ValidatorException;
  * @since 0.9.9.2
  */
 public abstract class TextAreaBox extends Autocompleter {
+
+    private static final String MODE_REMOTE = "remote";
+	
+	/**
+	 * @see org.apache.tapestry.dojo.form.Autocompleter#renderFormWidget(org.apache.tapestry.IMarkupWriter, org.apache.tapestry.IRequestCycle)
+	 */
+	@SuppressWarnings({ "unchecked", "unchecked" })
+	@Override
+	protected void renderFormWidget(IMarkupWriter writer, IRequestCycle cycle) {
+        renderDelegatePrefix(writer, cycle);
+        
+        writer.begin("select");
+        writer.attribute("name", getName());
+        
+        if (isDisabled())
+            writer.attribute("disabled", "disabled");
+        
+        renderIdAttribute(writer, cycle);
+        
+        renderDelegateAttributes(writer, cycle);
+        
+        getValidatableFieldSupport().renderContributions(this, writer, cycle);
+        
+        // Apply informal attributes.
+        renderInformalParameters(writer, cycle);
+        
+        writer.end();
+        renderDelegateSuffix(writer, cycle);
+        
+        ILink link = getDirectService().getLink(true, new DirectServiceParameter(this));
+        
+        Map parms = new HashMap();
+        parms.put("id", getClientId());
+        
+        JSONObject json = new JSONObject();
+        json.put("dataUrl", link.getURL() + "&filter=%{searchString}");
+        json.put("mode", MODE_REMOTE);
+        json.put("widgetId", getName());
+        json.put("name", getName());
+        json.put("searchDelay", getSearchDelay());
+        json.put("fadeTime", getFadeTime());
+        
+        IAutocompleteModel model = getModel();
+        if (model == null)
+            throw Tapestry.createRequiredParameterException(this, "model");
+        
+        Object value = getValue();
+        
+        if (value != null) {
+            
+            json.put("value", value.toString());
+            json.put("label", BeanUtils.getProperty(model,"cnlabel"));
+        }
+        
+        parms.put("props", json.toString());
+        parms.put("form", getForm().getName());
+        
+        PageRenderSupport prs = TapestryUtils.getPageRenderSupport(cycle, this);
+        getScript().execute(this, cycle, prs, parms);
+	}
 
 	/**
 	 * @see org.apache.tapestry.dojo.form.Autocompleter#renderComponent(org.apache.tapestry.json.IJSONWriter, org.apache.tapestry.IRequestCycle)
