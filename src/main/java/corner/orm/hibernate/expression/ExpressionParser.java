300c6494cfd9f370cfe62e438050369fc4b0fa5e
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-02 02:04:53 +0000

    完成了表达式的解析
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1593 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/hibernate/expression/ExpressionParser.java
+++ src/main/java/corner/orm/hibernate/expression/ExpressionParser.java
@@ -25,34 +25,72 @@ import java.util.regex.Pattern;
  * @since 2.1
  */
 public class ExpressionParser {
-	public final static String DATE_PATTERN="([\\d]*)~([\\d]*)";
+	/** 对日期类型的数据进行分析 **/
+	public final static String DATE_PATTERN="^([\\d]*)~([\\d]*)$";
+	/** 对表达式的模式进行分析 **/
+	public final static String EXP_PATTERN="^([^\\s]*)(([\\s]+(AND|OR)[\\s]+([^\\s]+))*)$";
+	/** 对表达式的字模式进行分析 **/
+	public final static String EXP_SUB_PATTER="[\\s]+(AND|OR)[\\s]+([^\\s]+)";
 	
-	public final static String EXP_PATTERN="^([^\\s]*)([\\s]+(AND|OR)[\\s]+([^\\s]+))*$";
-	public static  Iterator<ExpPair> parseExpression(String value){
+	static Pattern date_pattern=Pattern.compile(DATE_PATTERN,Pattern.CASE_INSENSITIVE );
+	private static Pattern str_pattern=Pattern.compile(EXP_PATTERN,Pattern.CASE_INSENSITIVE );
+	private static Pattern str_sub_pattern=Pattern.compile(EXP_SUB_PATTER,Pattern.CASE_INSENSITIVE);
+	/**
+	 * 对日期类型的数据进行分析
+	 * <p>返回为拆分候的数组，此数组有可能为一个有可能为两个.
+	 * @param input 输入的数据.
+	 * @return 分析完的表达式组.
+	 */
+	public static String[] parseDateExpression(String input){
+		Matcher matcher=date_pattern.matcher(input);
+		if(matcher.find()){
+			return new String[]{matcher.group(1),matcher.group(2)};
+		}else{
+			return new String[] {input};
+		}
+	}
+	/**
+	 * 对字符串进行解析,分析出来表达式.
+	 * <p>返回为一个ExpPair的集合。
+	 * @param input 待分析的值。
+	 * @return 分析完毕之后的表达式组.
+	 */
+	public static  Iterator<ExpPair> parseStringExpression(String input){
 		
+		Matcher matcher=str_pattern.matcher(input);
 		
-		Matcher matcher=Pattern.compile(EXP_PATTERN,Pattern.CASE_INSENSITIVE ).matcher(value);
-		
-		//声明匹配的组.
+		//声明匹配的结果组.
 		List<ExpPair> list=new ArrayList<ExpPair>();
-		
-		while(matcher.find()){
-			System.out.println("groupCount:"+matcher.groupCount());
+		if(matcher.find()){
 			ExpressionParser.ExpPair pair= new ExpressionParser.ExpPair();
-			pair.exp="and";
+			//第一个字符串为表达式为空
+			pair.exp=" ";
 			pair.value=matcher.group(1);
 			list.add(pair);
 			
-			if(matcher.group(3)!=null){
-				pair=new ExpressionParser.ExpPair();
-				pair.exp=matcher.group(3);
-				pair.value=matcher.group(4);
-				list.add(pair);
+			//如果有子串则对其进行分析
+			if(matcher.group(2)!=null){
+				doSubParse(list,matcher.group(2));
 			}
 			
 			
 		}
 		return list.iterator();
+	}
+	/**
+	 * 对子串进行分析.
+	 * @param list 匹配结果保存的列表.
+	 * @param str 待分析的子串.
+	 */
+	private static void doSubParse(List<ExpPair> list,String str) {
+		Matcher matcher=str_sub_pattern.matcher(str);
+		while(matcher.find()){//循环得到所有的子串.
+			ExpressionParser.ExpPair pair= new ExpressionParser.ExpPair();
+			pair.exp=matcher.group(1);
+			pair.value=matcher.group(2);
+			list.add(pair);
+		}
+		
 	}
 	public static class ExpPair{
 		public String exp;
