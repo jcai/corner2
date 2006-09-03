ca40a4495243605795e9e65d8e1e885b104bed7d
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-03 03:39:50 +0000

    写入基本测试用例
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1615 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/test/BaseFunctionTestCase.java
@@ -0,0 +1,117 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.test;
+
+import junit.framework.TestCase;
+
+import org.mortbay.http.SocketListener;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.servlet.WebApplicationContext;
+import org.openqa.selenium.server.SeleniumCommandTimedOutException;
+import org.openqa.selenium.server.browserlaunchers.AsyncExecute;
+import org.openqa.selenium.server.browserlaunchers.FirefoxCustomProfileLauncher;
+import org.openqa.selenium.server.htmlrunner.HTMLResultsListener;
+import org.openqa.selenium.server.htmlrunner.HTMLTestResults;
+import org.openqa.selenium.server.htmlrunner.SeleniumHTMLRunnerResultsHandler;
+
+/**
+ * 
+ * 基本的功能性质的测试.
+ * @author Jun Tsai
+ * @version $Revision$
+ * @since 2.1
+ */
+public abstract class  BaseFunctionTestCase extends TestCase implements HTMLResultsListener{
+	private org.mortbay.jetty.Server server = null;
+	private HTMLTestResults results;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		server=new Server();
+
+		SocketListener socketListener = new SocketListener();
+        socketListener.setPort(this.getListenerPort());
+        server.addListener(socketListener);
+
+        WebApplicationContext context=new WebApplicationContext();
+        context.setContextPath("/");
+        context.setWAR(this.getWebappDirectory());
+        SeleniumHTMLRunnerResultsHandler postResultsHandler = new SeleniumHTMLRunnerResultsHandler();
+        context.addHandler(postResultsHandler);
+        postResultsHandler.addListener(this);
+        
+        server.addContext(context);
+
+        server.start();
+
+	}
+	
+	protected String getWebappDirectory(){
+		return "src/main/webapp";
+	}
+	protected int getListenerPort(){
+		return 8888;
+	}
+	protected String getSeleinumTestUrl(){
+		return "http://localhost:"+this.getListenerPort()+"/selenium/TestRunner.html?test=TestSuite.html&auto=true&resultsUrl=/postResults";
+	    
+	}
+
+	public void testFunction() throws Exception {
+		
+		String sessionId = Long.toString(System.currentTimeMillis() % 1000000);
+		FirefoxCustomProfileLauncher  launcher=new FirefoxCustomProfileLauncher(8888,sessionId);
+		launcher.launch(this.getSeleinumTestUrl());
+//		long now = System.currentTimeMillis();
+//        long end = Integer.MAX_VALUE;
+        
+        
+        while (results == null ) {
+            AsyncExecute.sleepTight(500);
+        }
+        launcher.close();
+        if (results == null) {
+            throw new SeleniumCommandTimedOutException();
+        }
+//        if (outputFile != null) {
+//            FileWriter fw = new FileWriter(outputFile);
+//            results.write(fw);
+//            fw.close();
+//        }
+        
+		/*
+		List<String> list=new ArrayList<String>();
+		list.add("-htmlSuite");
+		list.add("*firefox");
+		list.add("http://localhost:8888");
+		list.add("src/main/webapp/selenium/TestSuite.html");
+		list.add("target/result.html");
+
+		try {
+			org.openqa.selenium.server.SeleniumServer.main(list.toArray(new String[0]));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		*/
+
+		
+	}
+	/** Accepts HTMLTestResults for later asynchronous handling */
+    public void processResults(HTMLTestResults resultsParm) {
+        this.results = resultsParm;
+    }
+
+	protected void tearDown() throws Exception {
+		server.stop();
+	}
+}
--- src/test/java/corner/function/FunctionTest.java
+++ src/test/java/corner/function/FunctionTest.java
@@ -1,89 +1,17 @@
 package corner.function;
 
-import junit.framework.TestCase;
+import corner.test.BaseFunctionTestCase;
 
-import org.mortbay.http.SocketListener;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.servlet.WebApplicationContext;
-import org.openqa.selenium.server.SeleniumCommandTimedOutException;
-import org.openqa.selenium.server.browserlaunchers.AsyncExecute;
-import org.openqa.selenium.server.browserlaunchers.FirefoxCustomProfileLauncher;
-import org.openqa.selenium.server.htmlrunner.HTMLResultsListener;
-import org.openqa.selenium.server.htmlrunner.HTMLTestResults;
-import org.openqa.selenium.server.htmlrunner.SeleniumHTMLRunnerResultsHandler;
+public class FunctionTest extends BaseFunctionTestCase{
 
-public class FunctionTest extends TestCase implements HTMLResultsListener{
-	private org.mortbay.jetty.Server server = null;
-	private HTMLTestResults results;
-
-	protected void setUp() throws Exception {
-		super.setUp();
-		server=new Server();
-
-		SocketListener socketListener = new SocketListener();
-        socketListener.setPort(8888);
-        server.addListener(socketListener);
-
-        WebApplicationContext context=new WebApplicationContext();
-        context.setContextPath("/");
-        context.setWAR("src/main/webapp");
-        SeleniumHTMLRunnerResultsHandler postResultsHandler = new SeleniumHTMLRunnerResultsHandler();
-        context.addHandler(postResultsHandler);
-        postResultsHandler.addListener(this);
-        
-        server.addContext(context);
-
-        server.start();
-
-	}
-
-	public void testFunction() throws Exception {
+	/**
+	 * @see corner.test.BaseFunctionTestCase#getListenerPort()
+	 */
+	@Override
+	protected int getListenerPort() {
 		
-		String url="http://localhost:8888/selenium/TestRunner.html?test=TestSuite.html&auto=true&resultsUrl=/postResults";
-	    String sessionId = Long.toString(System.currentTimeMillis() % 1000000);
-		FirefoxCustomProfileLauncher  launcher=new FirefoxCustomProfileLauncher(8888,sessionId);
-		launcher.launch(url);
-		long now = System.currentTimeMillis();
-        long end = Integer.MAX_VALUE;
-        
-        
-        while (results == null ) {
-            AsyncExecute.sleepTight(500);
-        }
-        launcher.close();
-        if (results == null) {
-            throw new SeleniumCommandTimedOutException();
-        }
-//        if (outputFile != null) {
-//            FileWriter fw = new FileWriter(outputFile);
-//            results.write(fw);
-//            fw.close();
-//        }
-        
-		/*
-		List<String> list=new ArrayList<String>();
-		list.add("-htmlSuite");
-		list.add("*firefox");
-		list.add("http://localhost:8888");
-		list.add("src/main/webapp/selenium/TestSuite.html");
-		list.add("target/result.html");
-
-		try {
-			org.openqa.selenium.server.SeleniumServer.main(list.toArray(new String[0]));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		*/
-
-		
+		return 8888;
 	}
-	/** Accepts HTMLTestResults for later asynchronous handling */
-    public void processResults(HTMLTestResults resultsParm) {
-        this.results = resultsParm;
-    }
-
-	protected void tearDown() throws Exception {
-		server.stop();
-	}
+	
 
 }
