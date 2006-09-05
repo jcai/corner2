bb2ff62b5620f987ef1930bd0d59ea7ae1d29f9a
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-05 01:21:45 +0000

    对TextAreaBox组件进行了重构
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1629 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/textareabox/TextAreaBox.java
+++ src/main/java/corner/orm/tapestry/component/textareabox/TextAreaBox.java
@@ -148,15 +148,48 @@ public abstract class TextAreaBox extends Autocompleter {
 
 	public IAutocompleteModel getModel(){
 		try {
-			return new DoubleSearchAutocompleteModel(this.getEntityService(),Class.forName(this.getQueryClass()),"name","cnName");
+			return new DoubleSearchAutocompleteModel(this.getEntityService(),Class.forName(this.getQueryClass()),this.getLabel(),this.getCnlabel());
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
+	/**
+	 * 设置下拉层列表中要显示的实体
+	 * @param queryClass
+	 */
 	 public abstract void setQueryClass(String queryClass);
+	 /**
+	  * 取得拉层列表中要显示的实体
+	  * @return
+	  */
 	 public abstract String getQueryClass();
+	 /**
+	  * 提供对实体的增删改查操作的能力
+	  * @return
+	  */
 	 public abstract EntityService getEntityService();
+	 /**
+	  * 取得供拼音检索的字段名称
+	  * @return
+	  */
+	 public abstract String getLabel();
+	 /**
+	  * 设置拼音检索的字段名称
+	  * @param label
+	  */
+	 public abstract void  setLabel(String label);
+	 
+	 /**
+	  * 设置中文检索的字段名称
+	  * @param cnlabel
+	  */
+	 public abstract void setCnlabel(String cnlabel);
+	 /**
+	  * 取得中文检索的字段名称
+	  * @return
+	  */
+	 public abstract String getCnlabel();
 
 }
