0113f073a4ddba17646ab5457367dad30a3e1480
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-11 08:09:31 +0000

    增加了CornerTextArea的静态样例
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1707 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/js/corner/widget/CornerTextArea.js
+++ src/main/js/corner/widget/CornerTextArea.js
@@ -1,22 +1,27 @@
+/*
+	Copyright (c) Beijing Maxinfo Technology Co.,Ltd.
+	All Rights Reserved.
+
+*/
+
 dojo.provide("corner.widget.CornerTextArea");
-
 dojo.require("dojo.widget.*");
-
 dojo.require("dojo.widget.Select");
 
+//定义一个CornerTextArea的widget.
+dojo.widget.defineWidget("corner.widget.CornerTextArea");
 dojo.widget.tags.addParseTreeHandler("dojo:CornerTextArea");
+
+//定义基本的CornerTextArea
 corner.widget.CornerTextArea = function(){
 	 dojo.widget.html.Select.call(this);
 }
-
+//继承
 dojo.inherits(corner.widget.CornerTextArea,  dojo.widget.html.Select);
-
+//对CornerTextArea进行扩展.
 dojo.lang.extend(corner.widget.CornerTextArea,{
 		widgetType: "CornerTextArea",
-		templatePath: dojo.uri.dojoUri("../corner/widget/templates/CornerTextArea.html"),
-		startSearchFromInput: function(){
-			dojo.debug(this.textInputNode.value);
-			//this.startSearch(this.textInputNode.value);
-		},
+		templatePath: dojo.uri.dojoUri("../corner/widget/templates/TextAreaBox.html"),
+		templateCssPath: dojo.uri.dojoUri("../corner/widget/templates/TextAreaBox.css")
 	
 });
