4bc801f48b43c1346edfd84708e747789f6921de
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-19 08:54:01 +0000

    初步完成了CornerTextArea组件的静态脚本
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1820 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/resources/corner/widget/CornerTextAreaBox.js
+++ src/test/resources/corner/widget/CornerTextAreaBox.js
@@ -1,4 +1,4 @@
-//期望每次通过json传递的字符串，用于提供TextAreaBox显示下拉层数据，以及检索
+//期望每次通过json传递的字符串，用于提供CornerTextAreaBox显示下拉层数据，以及检索
 //传递格式:{检索代码:中文说明,....}
 //其中，中文说明部分不变，只有减缩码需要变化为用户输入的value
-{"籍周润":["jzr1","jzr11","jzr111"],"蔡君1":["cj1","cj11","cj1111"],"宋艳豪":["syh","syh1","syh11"],"赵鑫":["zx1","zx11","zx111"],"蔡君2":["cj21","cj211","cj2111"],"蔡君":["cj1","cj11","cj111"]}
+{"籍周润":"客户名（中文）:籍周润,客户名（英文）:jzr,公司地址（中文）:北京美讯技术有限公司,公司地址（英文）:beijing bjmaxinfo company$com.bjmaxinfo.A@123456","蔡君1":"客户名（中文）:蔡君1,客户名（英文）:cj1,公司地址（中文）:北京美讯技术有限公司,公司地址（英文）:beijing bjmaxinfo company$com.bjmaxinfo.A@223456","宋艳豪":"客户名（中文）:宋艳豪,客户名（英文）:syh,公司地址（中文）:北京美讯技术有限公司,公司地址（英文）:beijing bjmaxinfo company$com.bjmaxinfo.A@323456","赵鑫":"客户名（中文）:赵鑫,客户名（英文）:zx,公司地址（中文）:北京美讯技术有限公司,公司地址（英文）:beijing bjmaxinfo company$com.bjmaxinfo.A@423456"}
