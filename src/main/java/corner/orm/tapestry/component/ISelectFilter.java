7f2eb501b4854bd4ae12ef0ec8c60a57fb217120
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-19 07:44:45 +0000

    增加了注释
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1813 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/ISelectFilter.java
+++ src/main/java/corner/orm/tapestry/component/ISelectFilter.java
@@ -17,6 +17,8 @@ import java.util.Map;
 import corner.service.EntityService;
 
 /**
+ * 提供自定义查询方法的接口
+ * <p>使用CornerSelect可以根据自己的需求定义查询方法类，但是该类必须实现本接口，如需复写，请参考DefaultSelectFilter</p>
  * @author Ghost
  * @version $Revision$
  * @since 2.1.1
@@ -33,33 +35,57 @@ public interface ISelectFilter {
 	public static final int nPageSize = 20;	
 	
 	/**
-	 * 
+	 * 根据用户的输入返回结果
 	 * @param match
 	 * @return
 	 */
 	public Map filterValues(String match) ;
 	
 	/**
-	 * 
+	 * 设置EntityService
 	 * @param entityService
 	 */
 	public void setEntityService(EntityService entityService);
 	
 	/**
-	 * 
+	 * 取得EntityService
 	 * @return
 	 */
 	public EntityService getEntityService();
 	
+	/**
+	 * 设置label
+	 * @param label
+	 */
 	public void setLabel(String label);
 	
+	/**
+	 * 返回label
+	 * @return
+	 */
 	public String getLabel();
 	
+	/**
+	 * 设置cnlabel
+	 * @param cnLabel
+	 */
 	public void setCnLabel(String cnLabel);
 	
+	/**
+	 * 返回cnlabel
+	 * @return
+	 */
 	public String getCnLabel();
 	
+	/**
+	 * 设置查询实体类
+	 * @param queryClass
+	 */
 	public void setQueryClass(Class queryClass);
 	
+	/**
+	 * 返回查询实体类
+	 * @return
+	 */
 	public Class getQueryClass();
 }
