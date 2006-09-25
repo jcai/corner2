92456784ddf30d6bd63eca597625967cca2c6048
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-25 03:28:37 +0000

    增加基础测试类的说明文字
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1905 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/test/AbstractTestCase.java
+++ src/main/java/corner/test/AbstractTestCase.java
@@ -6,8 +6,6 @@ import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 import corner.orm.spring.SpringContainer;
 import corner.service.EntityService;
@@ -19,29 +17,44 @@ import corner.service.EntityService;
  * @since 0.5.2
  */
 public class AbstractTestCase extends Assert {
+	//spring container
 	protected SpringContainer container = SpringContainer.getInstance();
+	//默认的事物定义
 	private TransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+	//事物管理的manager
 	HibernateTransactionManager manager;
 	TransactionStatus t;
 	
-	
+	/**
+	 * 启动一个事物
+	 *
+	 */
 	protected void startTransaction(){
 		manager=(HibernateTransactionManager) container.getApplicationContext().getBean("transactionManager");
         t=manager.getTransaction(transactionDefinition);
         
 	}
+	/**
+	 * 提交事物
+	 *
+	 */
 	protected void commitTransaction(){
 		manager.commit(t);
 	}
 	
-	
+	/**
+	 * 得到基础的EntityService.
+	 * @return 实体service
+	 */
 	protected EntityService getEntityService() {
 		return (EntityService) container.getApplicationContext().getBean(
 				"entityService");
 	}
-	
+	/**
+	 * 得到当前事物邦定的Session.
+	 * @return 当前的Session.
+	 */
 	protected Session getCurrentSession(){
-		
 		return manager.getSessionFactory().getCurrentSession();
 	}
 }
