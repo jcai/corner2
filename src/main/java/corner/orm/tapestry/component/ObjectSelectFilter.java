f933cb3c51adfe64695b187828c3c63ffaf4055a
Ghostbb <Ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-09-30 04:34:17 +0000

    增加了ObjectSelectFilter用于实现Autocompleter中的对象保存
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1942 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/tapestry/component/ObjectSelectFilter.java
@@ -0,0 +1,53 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.tapestry.component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ghost
+ * @version $Revision$
+ * @since 2.2.1
+ */
+public class ObjectSelectFilter extends DefaultSelectFilter {
+
+	/**
+	 * @see corner.orm.tapestry.component.DefaultSelectFilter#filterValues(java.lang.String)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public Map filterValues(String match) {
+		Map ret = new HashMap();
+        
+        if (match == null)
+            return ret;
+        
+        StringBuffer buffer = new StringBuffer("");
+        buffer.append(match.trim());
+        buffer.append("%");
+        String filter = buffer.toString();
+
+        List _values = this.listAllMatchedValue(filter);
+        for(Object obj:_values){
+
+        	//需要保存关联的时候使用
+        	Object label = obj;
+        	String cnlabel = this.getCnLabelFor(obj);
+        	ret.put(label, cnlabel);
+        }
+        return ret;
+	}
+
+}
--- src/test/java/corner/demo/model/many2many2/AB.java
+++ src/test/java/corner/demo/model/many2many2/AB.java
@@ -55,6 +55,7 @@ public class AB extends AbstractModel{
 	 */
 	private String message;
 	
+
 	/**
 	 * @return Returns the message.
 	 */
