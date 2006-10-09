6b657b87a02298181167e0411779b5356b563707
Ghostbb <Ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-10-09 08:38:53 +0000

    复写了Tapestry TextField组件,提供了一个指定默认值的能力
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1972 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/tapestry/component/textfield/TextField.java
@@ -0,0 +1,74 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.tapestry.component.textfield;
+
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+
+/**
+ * 复写Tapestry的TextField,提供指定TextField默认值的能力
+ * @author Ghost
+ * @version $Revision$
+ * @since 2.2.1
+ */
+public abstract class TextField extends org.apache.tapestry.form.TextField {
+
+	/**
+	 * @see org.apache.tapestry.form.TextField#renderFormComponent(org.apache.tapestry.IMarkupWriter, org.apache.tapestry.IRequestCycle)
+	 */
+	@Override
+	protected void renderFormComponent(IMarkupWriter writer, IRequestCycle cycle) {
+		if(this.getDefaultValue()==null || this.getDefaultValue().trim().length()<1){
+			super.renderFormComponent(writer, cycle);
+		}
+		else{
+			String value = getTranslatedFieldSupport().format(this, getValue());
+	        if(value != null && value.trim().length()>0){
+	        	super.renderFormComponent(writer, cycle);
+	        }
+	        else{
+
+		        renderDelegatePrefix(writer, cycle);
+
+		        writer.beginEmpty("input");
+
+		        writer.attribute("type", isHidden() ? "password" : "text");
+
+		        writer.attribute("name", getName());
+
+		        if (isDisabled()) writer.attribute("disabled", "disabled");
+
+		        writer.attribute("value", this.getDefaultValue());
+
+		        renderIdAttribute(writer, cycle);
+
+		        renderDelegateAttributes(writer, cycle);
+
+		        getTranslatedFieldSupport().renderContributions(this, writer, cycle);
+		        getValidatableFieldSupport().renderContributions(this, writer, cycle);
+
+		        renderInformalParameters(writer, cycle);
+
+		        writer.closeTag();
+
+		        renderDelegateSuffix(writer, cycle);
+	        }
+		}
+	}
+
+	/**
+	 * 取得默认值
+	 * @return
+	 */
+	public abstract String getDefaultValue();
+}
