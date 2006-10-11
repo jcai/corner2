45c66ae7a2a373b5432d51f77789ce9daecf354b
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-10-11 04:49:19 +0000

    纠正构建脚本的路径错误
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@1978 bda6c913-8cf9-0310-89cc-b5856a464ced

--- doc/dictionary/build.sh
+++ doc/dictionary/build.sh
@@ -1,1 +1,1 @@
-java -cp "../reference/support/lib/ant-launcher-1.6.5.jar" org.apache.tools.ant.launch.Launcher -lib ../reference/support/lib $*
+java -cp "../../support/lib/ant-launcher-1.6.5.jar" org.apache.tools.ant.launch.Launcher -lib ../../support/lib $*
--- doc/reference/build.bat
+++ doc/reference/build.bat
@@ -1,1 +1,1 @@
-java -cp "support\lib\ant-launcher-1.6.5.jar" org.apache.tools.ant.launch.Launcher -lib support\lib %1 %2 %3 %4 %5
+java -cp "..\..\support\lib\ant-launcher-1.6.5.jar" org.apache.tools.ant.launch.Launcher -lib ..\..\support\lib %1 %2 %3 %4 %5
