a353e46c39583a7db21e0301c9dffb9781bbb70d
Ghostbb <Ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-10-13 07:53:17 +0000

    复写Insert组件,让Insert组件显示的内容可以指定长度
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2003 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/tapestry/component/insert/Insert.java
@@ -0,0 +1,64 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.tapestry.component.insert;
+
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+
+/**
+ * 复写Tapestry的Insert,让Insert可以指定显示字符的长度,大于该长度的部分用...代替
+ * @author Ghost
+ * @version $Revision$
+ * @since 2.2.1
+ */
+public abstract class Insert extends org.apache.tapestry.components.Insert {
+
+	
+	/**
+	 * @see org.apache.tapestry.components.Insert#renderComponent(org.apache.tapestry.IMarkupWriter, org.apache.tapestry.IRequestCycle)
+	 */
+	@Override
+	protected void renderComponent(IMarkupWriter writer, IRequestCycle cycle) {
+		if(this.getValue()==null || this.getValue().toString().trim().length()<1){//如果value是空，或者value的长度小于1
+			super.renderComponent(writer, cycle);
+		}
+		else{
+			if(this.getLength()>0){//如果指定的长度大于0
+				if(this.getValue().toString()!=null && this.getValue().toString().length()>=this.getLength()){//指定的长度小于value的长度
+					StringBuffer buffer = new StringBuffer(this.getValue().toString().trim().substring(0, this.getLength()));
+					buffer.append("...");
+					this.setValue(buffer.toString());
+					super.renderComponent(writer, cycle);
+				}
+				else{
+					super.renderComponent(writer, cycle);
+				}
+			}
+			else{
+				super.renderComponent(writer, cycle);
+			}			
+		}
+	}
+
+	/**
+	 * 设置取得的value值
+	 * @param value
+	 */
+	public abstract void setValue(Object value);
+	
+	/**
+	 * 取得指定该字段显示的长度
+	 * @return
+	 */
+	public abstract int getLength();
+}
