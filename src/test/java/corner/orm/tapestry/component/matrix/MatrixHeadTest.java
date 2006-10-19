2f91e0ece6fda3e64d113d26b46cd00aaabd1728
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-10-19 07:40:21 +0000

    初步加入测试类，还没想好怎么测
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2053 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/test/java/corner/orm/tapestry/component/matrix/MatrixHeadTest.java
@@ -0,0 +1,33 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.tapestry.component.matrix;
+
+import org.apache.tapestry.BaseComponentTestCase;
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRender;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.components.Any;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:jun.tsai@bjmaxinfo.com">Jun Tsai</a>
+ * @version $Revision$
+ * @since 2.2.2
+ */
+public class MatrixHeadTest extends BaseComponentTestCase{
+	@Test
+	public void testRender(){
+		
+	}
+
+}
