6fa2dde90b0f4e6b2a130ef359d9ff9dafd8960a
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-10-26 09:08:57 +0000

    增加两个double类型的加法操作
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2100 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/util/CurrencyUtils.java
+++ src/main/java/corner/util/CurrencyUtils.java
@@ -14,36 +14,56 @@ package corner.util;
 
 import java.math.BigDecimal;
 
-
-
 /**
  * 转换货币的统一方法.
+ * 
  * @author <a href="mailto:jun.tsai@bjmaxinfo.com">Jun Tsai</a>
  * @version $Revision$
  * @since 2.2.2
  */
 public class CurrencyUtils {
-	/**默认为四舍五入方法**/
-	private static int DEFAULT_ROUND_PATTERN=BigDecimal.ROUND_HALF_UP;
+	/** 默认为四舍五入方法* */
+	private static int DEFAULT_ROUND_PATTERN = BigDecimal.ROUND_HALF_UP;
+
 	/**
-	 * 转换为货币。
-	 * 采用了四舍五入的方法.
-	 * @param currency 待转换的数字。
-	 * @param p 小数点后保留的位数.
+	 * 转换为货币。 采用了四舍五入的方法.
+	 * 
+	 * @param currency
+	 *            待转换的数字。
+	 * @param p
+	 *            小数点后保留的位数.
 	 * @return 转换后的数字.
 	 */
-	public  static double round(double currency,int p){
-		BigDecimal decimal=BigDecimal.valueOf(currency);
-		return decimal.divide(new BigDecimal(1),p,DEFAULT_ROUND_PATTERN).doubleValue();
+	public static double round(double currency, int p) {
+		BigDecimal decimal = BigDecimal.valueOf(currency);
+		return decimal.divide(new BigDecimal(1), p, DEFAULT_ROUND_PATTERN)
+				.doubleValue();
 	}
+
 	/**
 	 * 浮点类型的四舍五入.
-	 * @param currency 待转换的数字。
-	 * @param p 保留的位数。
+	 * 
+	 * @param currency
+	 *            待转换的数字。
+	 * @param p
+	 *            保留的位数。
 	 * @return 转换后的结果.
 	 */
-	public  static float round(float currency,int p){
-		BigDecimal decimal=BigDecimal.valueOf(currency);
-		return decimal.divide(new BigDecimal(1),p,DEFAULT_ROUND_PATTERN).floatValue();
+	public static float round(float currency, int p) {
+		BigDecimal decimal = BigDecimal.valueOf(currency);
+		return decimal.divide(new BigDecimal(1), p, DEFAULT_ROUND_PATTERN)
+				.floatValue();
 	}
+	/**
+	 * 提供了两个double类型的相加。
+	 * @param v1 值一
+	 * @param v2 值二
+	 * @param p 需要保留的小数位数.
+	 * @return 加后的结果.
+	 */
+	public static double plus(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.add(b2).doubleValue();//.divide(new BigDecimal(1), p, DEFAULT_ROUND_PATTERN).doubleValue();
 	}
+}
--- src/test/java/corner/util/CurrencyUtilsTest.java
+++ src/test/java/corner/util/CurrencyUtilsTest.java
@@ -25,14 +25,23 @@ import org.testng.annotations.Test;
 
 public class CurrencyUtilsTest {
 	@Test
-	public void testConvert(){
-		Assert.assertEquals(123456.12,CurrencyUtils.round((double)123456.123456,2));
-		Assert.assertEquals(123456.1235,CurrencyUtils.round((double)123456.123456,4));
-		Assert.assertEquals(-123456.1235,CurrencyUtils.round((double)-123456.123456,4));
-		Assert.assertEquals(-123456.11113,CurrencyUtils.round((double)-123456.111126,5));
-		Assert.assertEquals(1034.75,CurrencyUtils.round((double)1034.745,2));
-		Assert.assertEquals(1034.74,CurrencyUtils.round((double)1034.735,2));
-		
-		
+	public void testConvert() {
+		Assert.assertEquals(123456.12, CurrencyUtils.round(
+				(double) 123456.123456, 2));
+		Assert.assertEquals(123456.1235, CurrencyUtils.round(
+				(double) 123456.123456, 4));
+		Assert.assertEquals(-123456.1235, CurrencyUtils.round(
+				(double) -123456.123456, 4));
+		Assert.assertEquals(-123456.11113, CurrencyUtils.round(
+				(double) -123456.111126, 5));
+		Assert.assertEquals(1034.75, CurrencyUtils.round((double) 1034.745, 2));
+		Assert.assertEquals(1034.74, CurrencyUtils.round((double) 1034.735, 2));
+
 	}
+	@Test
+	public void testPlus(){
+		double v1=2.1;
+		double v2=2.2;
+		Assert.assertEquals(CurrencyUtils.plus(v1, v2),4.3);
 	}
+}
