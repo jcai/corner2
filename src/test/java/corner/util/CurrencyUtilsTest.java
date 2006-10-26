36efb33c18d0fc11d8f72f9a070983ca5f894faf
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-10-26 03:37:52 +0000

    加入对货币的四舍五入操作.
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2090 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/test/java/corner/util/CurrencyUtilsTest.java
@@ -0,0 +1,30 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.util;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:jun.tsai@bjmaxinfo.com">Jun Tsai</a>
+ * @version $Revision$
+ * @since 2.2.2
+ */
+@Test
+public class CurrencyUtilsTest {
+	public void testConvert(){
+		Assert.assertEquals(123456.12,CurrencyUtils.round((double)123456.123456,2));
+		Assert.assertEquals(123456.1235,CurrencyUtils.round((double)123456.123456,4));
+		Assert.assertEquals(-123456.1235,CurrencyUtils.round((double)-123456.123456,4));
+	}
+}
