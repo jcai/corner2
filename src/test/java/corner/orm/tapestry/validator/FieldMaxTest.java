487cada001d56f275e5353827ba736c218082ff1
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-11-10 01:54:39 +0000

    add test for fieldMax validator
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2179 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/orm/tapestry/validator/FieldMaxTest.java
+++ src/test/java/corner/orm/tapestry/validator/FieldMaxTest.java
@@ -2,6 +2,7 @@ package corner.orm.tapestry.validator;
 
 import java.util.Locale;
 
+import org.apache.tapestry.BaseComponentTestCase;
 import org.apache.tapestry.IPage;
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.form.IFormComponent;
@@ -16,7 +17,7 @@ import org.testng.annotations.Test;
  * @version $Revision$
  * @since 2.3
  */
-public class FieldMaxTest {
+public class FieldMaxTest extends BaseComponentTestCase{
 	
 	
 	public void testWrongValidate(){
@@ -38,5 +39,72 @@ public class FieldMaxTest {
 		}catch(ValidatorException e){
 			
 		}
+	}
+	@Test
+	public void testRightValidator(){
+		double d=12.0;
+		FieldMax max=new FieldMax();
+		String maxF="maxField";
+		double maxV=12;
+		max.setFieldMax(maxF);
+		
+		
+		
+		IFormComponent field=newMock(IFormComponent.class);
+		IPage page=newMock(IPage.class);
+		
+		EasyMock.expect(field.getPage()).andReturn(page);
+		
+		IRequestCycle cycle=newCycle();
+		EasyMock.expect(page.getRequestCycle()).andReturn(cycle);
+		
+		EasyMock.expect(cycle.getParameter(maxF)).andReturn(Double.toString(maxV));
+		
+		
+		ValidationMessages messages=null;
+		
+		replay();
+		try {
+			max.validate(field, messages, d);
+		} catch (ValidatorException e) {
+			fail("can't reacheable!");
+		}
+		verify();
+		
+	}
+	@Test
+	public void testWrongValidator(){
+		double d=12.0; 
+		FieldMax max=new FieldMax();
+		String maxF="maxField";
+		double maxV=100;
+		max.setFieldMax(maxF);
+		
+		
+		
+		IFormComponent field=newMock(IFormComponent.class);
+		IPage page=newMock(IPage.class);
+		
+		EasyMock.expect(field.getPage()).andReturn(page);
+		
+		IRequestCycle cycle=newCycle();
+		EasyMock.expect(page.getRequestCycle()).andReturn(cycle);
+		
+		EasyMock.expect(cycle.getParameter(maxF)).andReturn(Double.toString(maxV));
+		
+		String fieldName="fieldName";
+		EasyMock.expect(field.getDisplayName()).andReturn(fieldName);
+		
+		ValidationMessages messages=new ValidationMessagesImpl(field, Locale.getDefault());
+		
+		replay();
+		try {
+			max.validate(field, messages, d);
+			fail("can't reacheable!");
+		} catch (ValidatorException e) {
+			assertEquals(e.getMessage(),"必须小于或等于fieldName.");
+		}
+		verify();
+		
 	}
 }
