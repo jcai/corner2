8d26ec28cf66bd7272775886794d2beaa16250fd
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-11-10 04:54:58 +0000

    add test case for person id
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2186 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/orm/tapestry/validator/PersonIDTest.java
+++ src/test/java/corner/orm/tapestry/validator/PersonIDTest.java
@@ -1,16 +1,24 @@
 package corner.orm.tapestry.validator;
 
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.form.FormComponentContributorContext;
 import org.apache.tapestry.form.IFormComponent;
 import org.apache.tapestry.form.ValidationMessages;
 import org.apache.tapestry.form.ValidationMessagesImpl;
+import org.apache.tapestry.form.validator.BaseValidatorTestCase;
+import org.apache.tapestry.form.validator.Max;
+import org.apache.tapestry.json.JSONObject;
+import org.apache.tapestry.valid.ValidationStrings;
 import org.apache.tapestry.valid.ValidatorException;
 import org.easymock.EasyMock;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PersonIDTest {
+public class PersonIDTest extends BaseValidatorTestCase{
 	@Test
 	public void testWrongValidate(){
 		PersonID personID=new PersonID();
@@ -42,4 +50,35 @@ public class PersonIDTest {
 		}
 		EasyMock.verify(field);
 	}
+	@Test
+    public void testRenderContribution()
+    {
+        IMarkupWriter writer = newWriter();
+        IRequestCycle cycle = newCycle();
+        JSONObject json = new JSONObject();
+        
+        IFormComponent field = newField("My Field", "myfield");
+        
+        FormComponentContributorContext context = newMock(FormComponentContributorContext.class);
+        context.addInitializationScript(field,"dojo.require(\"corner.validate.web\");");
+        
+        Locale locale = Locale.FRANCE;
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
+        
+//        EasyMock.expect(context.getLocale()).andReturn(locale);
+        
+        EasyMock.expect(context.getProfile()).andReturn(json);
+        
+//        EasyMock.expect(context.formatValidationMessage("{0}不正确.格式为15／18位身份证号码.", null, new Object[]{"My Field"})).andReturn("");
+        trainFormatMessage(context,"{0}不正确.格式为15／18位身份证号码.",null,new Object[]{"My Field"},"My Field不正确.格式为15／18位身份证号码.");
+        replay();
+        
+        new PersonID().renderContribution(writer, cycle, context, field);
+
+        verify();
+        
+        assertEquals("{\"constraints\":{\"myfield\":[[corner.validate.isPersonID,false,true]]},"
+                + "\"myfield\":{\"constraints\":[\"My Field不正确.格式为15／18位身份证号码.\"]}}",
+                json.toString());
+    }
 }
