0f45e4191d55a88fc71e8700a4d19d40a4b67006
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-11-24 07:55:16 +0000

    反射的key converter
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2364 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/test/java/corner/orm/tapestry/ReflectPrimaryKeyConverterTest.java
@@ -0,0 +1,24 @@
+package corner.orm.tapestry;
+
+import org.apache.tapestry.BaseComponentTestCase;
+import org.easymock.EasyMock;
+import org.testng.annotations.Test;
+
+import corner.demo.model.one.A;
+
+public class ReflectPrimaryKeyConverterTest extends BaseComponentTestCase{
+	@Test
+	public void test_getPrimaryKey(){
+		A a=new A();
+		a.setId("id1");
+		a.setName("name");
+		
+		ReflectPrimaryKeyConverter<A> converter=new ReflectPrimaryKeyConverter<A>(A.class,"id");
+		assertEquals("id1",converter.getPrimaryKey(a));
+		A a2=(A) converter.getValue("id1");
+		assertEquals("id1",a2.getId());
+		
+		
+	}
+
+}
