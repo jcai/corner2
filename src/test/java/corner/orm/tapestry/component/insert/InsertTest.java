b0ca06e9c8257c3abca4b3a048f1d731b5681e64
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-11-24 08:07:42 +0000

    增加对Insert的测试
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2366 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/test/java/corner/orm/tapestry/component/insert/InsertTest.java
@@ -0,0 +1,51 @@
+package corner.orm.tapestry.component.insert;
+
+import org.apache.tapestry.BaseComponentTestCase;
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.testng.annotations.Test;
+
+import static org.easymock.EasyMock.*;
+
+public class InsertTest extends BaseComponentTestCase {
+	@Test
+	public void testNoFormat() {
+		IMarkupWriter writer = newWriter();
+		IRequestCycle cycle = newCycle(false);
+
+		Insert insert = newInstance(Insert.class, new Object[] { "value",
+				"123456789", "length",5});
+
+		expect(cycle.renderStackPush(insert)).andReturn(insert);
+
+		writer.print("12345...", false);
+
+		expect(cycle.renderStackPop()).andReturn(insert);
+
+		replay();
+
+		insert.render(writer, cycle);
+
+		verify();
+	}
+	@Test
+	public void test_with_ChieseFormat() {
+		IMarkupWriter writer = newWriter();
+		IRequestCycle cycle = newCycle(false);
+
+		Insert insert = newInstance(Insert.class, new Object[] { "value",
+				"我123456", "length",5});
+
+		expect(cycle.renderStackPush(insert)).andReturn(insert);
+
+		writer.print("我1234...", false);
+
+		expect(cycle.renderStackPop()).andReturn(insert);
+
+		replay();
+
+		insert.render(writer, cycle);
+
+		verify();
+	}
+}
