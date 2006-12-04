102c625623a1413c639e68e2cd4137b23a3aeaa6
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2006-12-04 01:51:23 +0000

    修正QueryBox
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2400 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/textfield/QueryBoxField.java
+++ src/main/java/corner/orm/tapestry/component/textfield/QueryBoxField.java
@@ -20,7 +20,10 @@ import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.IScript;
 import org.apache.tapestry.PageRenderSupport;
 import org.apache.tapestry.TapestryUtils;
+import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.InjectScript;
+import org.apache.tapestry.annotations.Parameter;
+import org.apache.tapestry.engine.IEngineService;
 import org.apache.tapestry.json.JSONObject;
 
 
@@ -32,6 +35,13 @@ import org.apache.tapestry.json.JSONObject;
  */
 public abstract class QueryBoxField extends TextField  {
 
+	
+	@Parameter(required=true)
+	public abstract String getQueryPageName();
+	
+	@InjectObject("service:tapestry.services.Page")
+	public abstract IEngineService getPageService();
+	
 	/**
 	 * @see corner.orm.tapestry.component.textfield.TextField#renderFormComponent(org.apache.tapestry.IMarkupWriter, org.apache.tapestry.IRequestCycle)
 	 */
@@ -41,7 +51,10 @@ public abstract class QueryBoxField extends TextField  {
 		 Map<String,String> parms=new HashMap<String,String>(); 
 		  parms.put("id", getClientId());
 		  JSONObject json=new JSONObject();
+		  //widgetID
 		  json.put("widgetId",getClientId());
+		  //构造查询页面的URL
+		  json.put("url",this.getPageService().getLink(false, this.getQueryPageName()));
 		  parms.put("props", json.toString());
 		 
 		 PageRenderSupport prs = TapestryUtils.getPageRenderSupport(cycle, this);
