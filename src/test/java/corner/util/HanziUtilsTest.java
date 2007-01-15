328c427316b4d235dc4e1e4d307fb78a388926b4
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-01-15 04:27:33 +0000

    多音字的测试
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2563 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/util/HanziUtilsTest.java
+++ src/test/java/corner/util/HanziUtilsTest.java
@@ -19,15 +19,18 @@ import junit.framework.TestCase;
 import org.testng.annotations.Test;
 @Test
 public class HanziUtilsTest extends TestCase {
-	public void testLoadHanzi() throws IOException{
-		String s=HanziUtils.getPinyin("asdf");
-		assertEquals("asdf",s);
-		s=HanziUtils.getPinyin("中华人民共和国");
-		assertEquals("zhrmghg",s);
-	}
-	public void test_hanzi(){
-		assertTrue(HanziUtils.isHanzi('我'));
-		assertFalse(HanziUtils.isHanzi('a'));
-		
+//	public void testLoadHanzi() throws IOException{
+//		String s=HanziUtils.getPinyin("asdf");
+//		assertEquals("asdf",s);
+//		s=HanziUtils.getPinyin("中华人民共和国");
+//		assertEquals("zhrmghg",s);
+//	}
+//	public void test_hanzi(){
+//		assertTrue(HanziUtils.isHanzi('我'));
+//		assertFalse(HanziUtils.isHanzi('a'));
+//		
+//	}
+	public void test_changsha(){
+		assertEquals("z",HanziUtils.getPinyin("长"));
 	}
 }
