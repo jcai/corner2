4e97f096b74a355919c015c36f287c64a575d8bd
Ghostbb <Ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-01-16 04:16:58 +0000

    对UniqueEntity进行了简单的refactor,增加了可以自定义查询条件的能力
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2565 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/validator/UniqueEntity.java
+++ src/main/java/corner/orm/tapestry/validator/UniqueEntity.java
@@ -68,17 +68,91 @@ public class UniqueEntity extends BaseValidator {
 		if (object == null) {
 			return;
 		}
-		Object obj=((EntityPage) field.getPage()).getEntity();
-		
-		//判断是否为更新操作.
-		if(getEntityService().isPersistent(obj)){//是否为持久化
-			Object properyValue=BeanUtils.getProperty(obj,this.property);
-			if(object.equals(properyValue)){//加入属性没变化，则不进行校验.
+		Object obj = ((EntityPage) field.getPage()).getEntity();
+
+		// 判断是否为更新操作.
+		if (getEntityService().isPersistent(obj)) {// 是否为持久化
+			Object properyValue = BeanUtils.getProperty(obj, this.property);
+			if (object.equals(properyValue)) {// 加入属性没变化，则不进行校验.
 				return;
 			}
 		}
-		//得到行数.
-		int rowCount = (Integer) ((HibernateDaoSupport) getEntityService()
+		// 得到行数.
+		int rowCount = this.getRowCount(field, object, getEntityClassName(),
+				getPropertyName());
+		// 抛出验证异常
+		if (rowCount > 0) {
+			throw new ValidatorException(messages.formatValidationMessage(
+					"{0}为" + object + "的记录已经存在", null, new Object[] { field
+							.getDisplayName() }));
+		}
+
+	}
+
+	public void setUniqueEntity(String uniqueStr) {
+
+		if (uniqueStr == null) {
+			exceptionAdvertiser(uniqueStr);
+		}
+		this.uniqueStr = uniqueStr;
+
+		initProperty();
+	}
+
+	protected void initProperty() {
+		Matcher matcher = Pattern.compile(REGEX_PATTERN)
+				.matcher(this.uniqueStr);
+		if (matcher.groupCount() != 2) {
+			exceptionAdvertiser(uniqueStr);
+		}
+		if (matcher.find()) {
+			this.entityClassName = matcher.group(1);
+			this.property = matcher.group(2);
+		}
+
+	}
+
+	/**
+	 * 设定待查询的类名
+	 * 
+	 * @param entityClassName
+	 */
+	public void setEntityClassName(String entityClassName) {
+		this.entityClassName = entityClassName;
+	}
+
+	/**
+	 * 设定查询的属性名称。
+	 * 
+	 * @param property
+	 *            属性名称.
+	 */
+	public void setCriterionProName(String property) {
+		this.property = property;
+	}
+
+	protected EntityService getEntityService() {
+		return (EntityService) SpringContainer.getInstance()
+				.getApplicationContext().getBean("entityService");
+	}
+
+	String getEntityClassName() {
+		return this.entityClassName;
+	}
+
+	String getPropertyName() {
+		return this.property;
+	}
+
+	/**
+	 * 查询给定条件的实体是否存在(这个方法可以方便用户自己扩展自己的查询条件)
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	protected int getRowCount(final IFormComponent field, final Object inputObj,
+			final String entityClassName, final String property) {
+		return (Integer) ((HibernateDaoSupport) getEntityService()
 				.getObjectRelativeUtils()).getHibernateTemplate().execute(
 				new HibernateCallback() {
 					public Object doInHibernate(Session session)
@@ -86,61 +160,25 @@ public class UniqueEntity extends BaseValidator {
 						Criteria criteria = session
 								.createCriteria(entityClassName);
 						criteria.setProjection(Projections.rowCount());
-						criteria.add(Restrictions.eq(property, object));
+						criteria.add(Restrictions.eq(property, inputObj));
 						return criteria.list().get(0);
 					}
 				});
-		//抛出验证异常
-		if (rowCount == 1) {
-			throw new ValidatorException(messages.formatValidationMessage(
-					"{0}为"+object+"的记录已经存在", null,
-					new Object[] { field.getDisplayName() }));
-		}
-
 	}
-	public void setUniqueEntity(String uniqueStr){
-		
-		if(uniqueStr==null){
-			throw new ApplicationRuntimeException("UniqueEntity validator的参数'"+uniqueStr+"'不对，应该为{className:propertyName}");
-		}
-		this.uniqueStr=uniqueStr;
-		
-		initProperty();
-	}
-	void initProperty(){
-		Matcher matcher=Pattern.compile(REGEX_PATTERN).matcher(this.uniqueStr);
-		if(matcher.groupCount()!=2){
-			throw new ApplicationRuntimeException("UniqueEntity validator的参数'"+uniqueStr+"'不对，应该为'{className:propertyName}'");
-		}
-		if(matcher.find()){
-			this.entityClassName=matcher.group(1);
-			this.property=matcher.group(2);
-		}
-		
-	}
+	
 	/**
-	 * 设定待查询的类名
-	 * @param entityClassName
+	 * 根据给定的uniqueStr抛出一个ApplicationRuntimeException
+	 * @param uniqueStr
 	 */
-	public void setEntityClassName(String entityClassName) {
-		this.entityClassName = entityClassName;
-	}
-	/**
-	 * 设定查询的属性名称。
-	 * @param property 属性名称.
-	 */
-	public void setCriterionProName(String property) {
-		this.property = property;
+	protected void exceptionAdvertiser(String uniqueStr){
+		throw new ApplicationRuntimeException("UniqueEntity validator的参数'"
+				+ uniqueStr + "'不对，应该为'{className:propertyName}'");
 	}
 
-	private EntityService getEntityService() {
-		return (EntityService) SpringContainer.getInstance()
-				.getApplicationContext().getBean("entityService");
-	}
-	String getEntityClassName(){
-		return this.entityClassName;
-	}
-	String getPropertyName(){
-		return this.property;
+	/**
+	 * @param uniqueStr The uniqueStr to set.
+	 */
+	public void setUniqueStr(String uniqueStr) {
+		this.uniqueStr = uniqueStr;
 	}
 }
