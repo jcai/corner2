8dc8b712e6ea32686abe4614dcf8ad827491dfb4
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-01-22 03:40:40 +0000

    增加对含有空格的测试
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2577 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/util/HanziUtilsTest.java
+++ src/test/java/corner/util/HanziUtilsTest.java
@@ -14,23 +14,29 @@ package corner.util;
 
 import java.io.IOException;
 
-import junit.framework.TestCase;
 
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 @Test
-public class HanziUtilsTest extends TestCase {
-//	public void testLoadHanzi() throws IOException{
-//		String s=HanziUtils.getPinyin("asdf");
-//		assertEquals("asdf",s);
-//		s=HanziUtils.getPinyin("中华人民共和国");
-//		assertEquals("zhrmghg",s);
-//	}
-//	public void test_hanzi(){
-//		assertTrue(HanziUtils.isHanzi('我'));
-//		assertFalse(HanziUtils.isHanzi('a'));
-//		
-//	}
+public class HanziUtilsTest extends Assert {
+	public void testLoadHanzi() throws IOException{
+		String s=HanziUtils.getPinyin("asdf");
+		assertEquals("asdf",s);
+		s=HanziUtils.getPinyin("中华人民共和国");
+		assertEquals("zhrmghg",s);
+	}
+	public void test_hanzi(){
+		assertTrue(HanziUtils.isHanzi('我'));
+		assertFalse(HanziUtils.isHanzi('a'));
+		
+	}
 	public void test_changsha(){
 		assertEquals("z",HanziUtils.getPinyin("长"));
+	}
+	public void test_whitespace(){
+		assertEquals("za asdf",HanziUtils.getPinyin("长a asdf"));
+		assertEquals("za nh",HanziUtils.getPinyin("长a 你好"));
+		assertEquals("za 　nh",HanziUtils.getPinyin("长a 　你好"));
 	}
 }
