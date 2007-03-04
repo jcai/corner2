cd496c8004c28905a9bbf3517bac8c06db46b29b
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-03-04 15:41:53 +0000

    动态增加table行记录
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2689 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/resources/corner/widget/show1.js
+++ src/test/resources/corner/widget/show1.js
@@ -4,3 +4,25 @@ function aaa(){
 	dojo.debug("a:" + a);
 	a.insertCell();
 }
+function foo(){
+	
+	var oTr = dojo.byId('myTable').firstChild.firstChild.cloneNode(true);
+	oTr.flag = "new";
+	dojo.byId('myTable').firstChild.appendChild(oTr);
+}
+function moveOver(){
+	var oTr = event.srcElement;
+	while(oTr.tagName != "TR"){
+		oTr = oTr.parentElement;
+	}
+	if(oTr.flag == "new")
+		oTr.style.backgroundColor='blue';
+}
+function moveOut(){
+	var oTr = event.srcElement;
+	while(oTr.tagName != "TR"){
+		oTr = oTr.parentElement;
+	}
+	if(oTr.flag == "new")
+		oTr.style.backgroundColor='';
+}
