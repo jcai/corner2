8bb0964d3b7288f1c393fa440196278693229264
Ghostbb <Ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-04-24 03:37:58 +0000

    保留corner:TextField的readOnly参数,因为corner:MatrixRow组件需要这个参数 控制所有生成的TextField为readOnly
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2802 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/textfield/TextField.java
+++ src/main/java/corner/orm/tapestry/component/textfield/TextField.java
@@ -17,7 +17,7 @@ import org.apache.tapestry.IRequestCycle;
 
 /**
  * 复写Tapestry的TextField,提供指定TextField默认值的能力
- * @author <a href=mailto:ghostbb@bjmaxinfo.com>Ghostbb</a>
+ * @author Ghost
  * @version $Revision$
  * @since 2.2.1
  */
@@ -29,6 +29,50 @@ public abstract class TextField extends org.apache.tapestry.form.TextField {
 	@Override
 	protected void renderFormComponent(IMarkupWriter writer, IRequestCycle cycle) {
 		Object defaultValue = this.getDefaultValue();
+		boolean isReadOnly = this.getReadOnly() != null ?this.getReadOnly().booleanValue():false; 
+		if(isReadOnly){
+			String value = null;
+			if(defaultValue==null || defaultValue.toString().trim().length()<1){
+				value = getTranslatedFieldSupport().format(this, getValue());
+			}
+			else{
+		        if(value != null && value.trim().length()>0){
+		        	value = getTranslatedFieldSupport().format(this, getValue());
+		        }
+		        else{
+		        	value = getTranslatedFieldSupport().format(this, this.getDefaultValue());
+		        }
+			}
+	        renderDelegatePrefix(writer, cycle);
+
+	        writer.beginEmpty("input");
+
+	        writer.attribute("type", isHidden() ? "password" : "text");
+
+	        writer.attribute("name", getName());
+
+	        if (isDisabled()) 
+	            writer.attribute("disabled", "disabled");
+
+	        if (value != null) 
+	            writer.attribute("value", value);
+	        
+	        //把该TextArea设置成ReadOnly
+	        writer.attribute("readonly", "true");
+
+	        renderIdAttribute(writer, cycle);
+
+	        renderDelegateAttributes(writer, cycle);
+
+	        getTranslatedFieldSupport().renderContributions(this, writer, cycle);
+	        getValidatableFieldSupport().renderContributions(this, writer, cycle);
+
+	        renderInformalParameters(writer, cycle);
+	        
+	        writer.closeTag();
+
+	        renderDelegateSuffix(writer, cycle);
+		} else{
 			if(defaultValue==null || defaultValue.toString().trim().length()<1){
 				super.renderFormComponent(writer, cycle);
 			}
@@ -38,11 +82,12 @@ public abstract class TextField extends org.apache.tapestry.form.TextField {
 		        if(value != null && value.trim().length()>0){
 		        	super.renderFormComponent(writer, cycle);
 		        }
-		        else{
+		        else{//TODO  为什么不能直接使用 setValue(getDefaultValue()) 这样岂不更简单？
 		        	this.setValue(this.getDefaultValue());
 		        	super.renderFormComponent(writer, cycle);
 		        }
 			}
+		}
 	}
 
 	/**
@@ -50,4 +95,11 @@ public abstract class TextField extends org.apache.tapestry.form.TextField {
 	 * @return
 	 */
 	public abstract Object getDefaultValue();
+	
+	/**
+	 * 取得该TextField的属性
+	 * true:该TextField为readonly;false:该TextField不是readonly
+	 * @return
+	 */
+	public abstract Boolean getReadOnly();
 }
