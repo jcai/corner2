6a732d17a33eded6e40764c4faa1d416c3822653
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-06-19 03:11:15 +0000

    增加GainPoint组建，完成展示和修改name
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@2942 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/gain/GainPoint.java
+++ src/main/java/corner/orm/tapestry/component/gain/GainPoint.java
@@ -18,6 +18,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.tapestry.BaseComponent;
+import org.apache.tapestry.IForm;
 import org.apache.tapestry.IMarkupWriter;
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.IScript;
@@ -27,7 +28,7 @@ import org.apache.tapestry.annotations.InjectScript;
 import org.apache.tapestry.annotations.Parameter;
 import org.apache.tapestry.form.IFormComponent;
 import org.apache.tapestry.form.Submit;
-import org.apache.tapestry.spec.IContainedComponent;
+import org.apache.tapestry.valid.IValidationDelegate;
 
 /**
  * 
@@ -35,14 +36,119 @@ import org.apache.tapestry.spec.IContainedComponent;
  * @version $Revision$
  * @since 2.3.7
  */
-public abstract class GainPoint extends BaseComponent{
+public abstract class GainPoint extends BaseComponent implements IFormComponent {
+
+	/**
+	 * Invoked from {@link #renderComponent(IMarkupWriter, IRequestCycle)} to
+	 * rewind the component. If the component is
+	 * {@link IFormComponent#isDisabled() disabled} this will not be invoked.
+	 * 
+	 * @param writer
+	 * @param cycle
+	 */
+	protected void rewindFormComponent(IMarkupWriter writer, IRequestCycle cycle){
+		String sl[] = cycle.getParameters("inName");
+		
+		for(String s : sl){
+			System.out.println(s);
+		}
+	}
 	
+	/**
+	 * @see org.apache.tapestry.form.AbstractFormComponent#renderFormComponent(org.apache.tapestry.IMarkupWriter,
+	 *      org.apache.tapestry.IRequestCycle)
+	 */
+	@Override
+	protected void renderComponent(IMarkupWriter writer, IRequestCycle cycle) {
+
+		Set entries = cycle.getPage().getComponents().entrySet();
+
+		Iterator iter = entries.iterator();
+
+		StringBuffer reNameTemps = new StringBuffer("{\\\"reNames\\\": [");
+
+		String reNames = null;
+
+		while (iter.hasNext()) {
+			Map.Entry entry = (Map.Entry) iter.next();
+			// Object key = entry.getKey();
+
+			if (!(entry.getValue() instanceof IFormComponent)) {
+				continue;
+			}
+			
+			if((entry.getValue() instanceof Submit) || (entry.getValue() instanceof GainPoint)){
+				continue;
+			}
+
+			IFormComponent component = (IFormComponent) entry.getValue();
+
+			// if(cc.getBinding("id").equals(this.getClientId()) ){
+			reNameTemps.append("\\\"").append(component.getClientId()).append(
+					"\\\",");
+			// }
+		}
+
+		reNames = reNameTemps.toString().substring(0, reNameTemps.length() - 1);
+
+		reNames += "]}";
+
+		
+		/**
+		 * 处理form
+		 */
+		IForm form = TapestryUtils.getForm(cycle, this);
+
+		setForm(form);
+
+		if (form.wasPrerendered(writer, this))
+			return;
+
+		IValidationDelegate delegate = form.getDelegate();
+
+		delegate.setFormComponent(this);
+
+		setName(form);
+
+		/**
+		 * 当提交时和为显示时
+		 */
+		if (form.isRewinding()) {
+			if (!isDisabled()) {
+				rewindFormComponent(writer, cycle);
+			}
+
+			// This is for the benefit of the couple of components (LinkSubmit)
+			// that allow a body.
+			// The body should render when the component rewinds.
+
+			if (getRenderBodyOnRewind())
+				renderBody(writer, cycle);
+		} if (!cycle.isRewinding()){
+			super.renderComponent(writer, cycle);
+        }
+
+		/**
+		 * 原来的处理name
+		 */
+		PageRenderSupport pageRenderSupport = TapestryUtils
+				.getPageRenderSupport(cycle, this);
+
+		Map<String, Object> scriptParms = new HashMap<String, Object>();
+
+		scriptParms.put("gpid", this.getClientId());
+		scriptParms.put("elementName", this.getElementName());
+		scriptParms.put("reNames", reNames);
+
+		getScript().execute(this, cycle, pageRenderSupport, scriptParms);
+	}
+
 	/**
 	 * @return
 	 */
 	@InjectScript("GainPoint.script")
 	public abstract IScript getScript();
-	
+
 	/**
 	 * 
 	 */
@@ -50,73 +156,17 @@ public abstract class GainPoint extends BaseComponent{
 	public abstract String getElementName();
 	
 	/**
-	 * @see org.apache.tapestry.form.AbstractFormComponent#renderFormComponent(org.apache.tapestry.IMarkupWriter, org.apache.tapestry.IRequestCycle)
+	 * 
 	 */
-	@Override
-	protected void renderComponent(IMarkupWriter writer, IRequestCycle cycle) {
-		
-		Set entries = cycle.getPage().getComponents().entrySet();
-		
-		Iterator iter = entries.iterator();
-		
-		StringBuffer reNameTemps = new StringBuffer("{\\\"reNames\\\": [");
-		
-		String reNames = null;
-		
-		while(iter.hasNext()){
-			Map.Entry entry = (Map.Entry)iter.next();
-//			Object key = entry.getKey();
-			
-			
-			if(!(entry.getValue() instanceof IFormComponent) || (entry.getValue() instanceof Submit)){
-				continue;
-			}
-			
-			IFormComponent component = (IFormComponent)entry.getValue();
-			
-			IContainedComponent cc = component.getContainedComponent();
-			
-//			if(cc.getBinding("id").equals(this.getClientId()) ){
-				reNameTemps.append("\\\"").append(component.getClientId()).append("\\\",");
-//			}
-		}
-		
-		reNames = reNameTemps.toString().substring(0,reNameTemps.length()-1);
-		
-		reNames += "]}";
-		
-		super.renderComponent(writer, cycle);
-		
-		PageRenderSupport pageRenderSupport = TapestryUtils.getPageRenderSupport(cycle, this);
-		
-		Map<String, Object> scriptParms = new HashMap<String, Object>();
-		
-		
-		scriptParms.put("gpid",		  this.getClientId());
-		scriptParms.put("elementName",		  this.getElementName());
-		scriptParms.put("reNames",		  reNames);
-		
-		getScript().execute(this, cycle, pageRenderSupport, scriptParms);
-		
-		
+	public abstract IForm getForm();
+
+	public abstract void setForm(IForm form);
+
+	protected boolean getRenderBodyOnRewind() {
+		return false;
 	}
-	
-	
 
-//	/**
-//	 * @see org.apache.tapestry.form.AbstractFormComponent#rewindFormComponent(org.apache.tapestry.IMarkupWriter, org.apache.tapestry.IRequestCycle)
-//	 */
-//	@Override
-//	protected void rewindFormComponent(IMarkupWriter writer, IRequestCycle cycle) {
-//		
-//		
-//		
-//		String[] values = cycle.getParameters(getName());
-//		
-//		for(String s : values){
-//			System.out.println(s);
-//		}
-//	}
-
-	
+	protected void setName(IForm form) {
+		setName(form.getElementId(this));
+	}
 }
