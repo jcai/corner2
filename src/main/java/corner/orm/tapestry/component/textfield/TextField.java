f38575759d048e9882eaebad00ee08d9014b3c84
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-07-09 06:58:51 +0000

    修正了corner:TextField的defaultValue的bug
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3125 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/textfield/TextField.java
+++ src/main/java/corner/orm/tapestry/component/textfield/TextField.java
@@ -17,6 +17,7 @@ import org.apache.tapestry.IRequestCycle;
 
 /**
  * 复写Tapestry的TextField,提供指定TextField默认值的能力
+ * 
  * @author Ghost
  * @version $Revision$
  * @since 2.2.1
@@ -24,79 +25,85 @@ import org.apache.tapestry.IRequestCycle;
 public abstract class TextField extends org.apache.tapestry.form.TextField {
 
 	/**
-	 * @see org.apache.tapestry.form.TextField#renderFormComponent(org.apache.tapestry.IMarkupWriter, org.apache.tapestry.IRequestCycle)
+	 * @see org.apache.tapestry.form.TextField#renderFormComponent(org.apache.tapestry.IMarkupWriter,
+	 *      org.apache.tapestry.IRequestCycle)
 	 */
 	@Override
 	protected void renderFormComponent(IMarkupWriter writer, IRequestCycle cycle) {
 		Object defaultValue = this.getDefaultValue();
-		boolean isReadOnly = this.getOnlyRead() != null ?this.getOnlyRead().booleanValue():false; 
-		if(isReadOnly){
+		boolean isReadOnly = this.getOnlyRead() != null ? this.getOnlyRead()
+				.booleanValue() : false;
+		if (isReadOnly) {
 			String value = null;
-			if(defaultValue==null || defaultValue.toString().trim().length()<1){
+			if (defaultValue == null || defaultValue.toString().trim().length() < 1) {
 				value = getTranslatedFieldSupport().format(this, getValue());
+			} else {
+				if (this.getValue() != null) {
+					value = getTranslatedFieldSupport()
+							.format(this, getValue());
+				} else {
+					value = getTranslatedFieldSupport().format(this,
+							this.getDefaultValue());
+				}
 			}
-			else{
-		        if(value != null && value.trim().length()>0){
-		        	value = getTranslatedFieldSupport().format(this, getValue());
-		        }
-		        else{
-		        	value = getTranslatedFieldSupport().format(this, this.getDefaultValue());
-		        }
-			}
-	        renderDelegatePrefix(writer, cycle);
+			renderDelegatePrefix(writer, cycle);
 
-	        writer.beginEmpty("input");
+			writer.beginEmpty("input");
 
-	        writer.attribute("type", isHidden() ? "password" : "text");
+			writer.attribute("type", isHidden() ? "password" : "text");
 
-	        writer.attribute("name", getName());
+			writer.attribute("name", getName());
 
-	        if (isDisabled()) 
-	            writer.attribute("disabled", "disabled");
+			if (isDisabled())
+				writer.attribute("disabled", "disabled");
 
-	        if (value != null) 
-	            writer.attribute("value", value);
-	        
-	        //把该TextArea设置成ReadOnly
-	        writer.attribute("readonly", "true");
+			if (value != null)
+				writer.attribute("value", value);
 
-	        renderIdAttribute(writer, cycle);
+			// 把该TextArea设置成ReadOnly
+			writer.attribute("readonly", "true");
 
-	        renderDelegateAttributes(writer, cycle);
+			renderIdAttribute(writer, cycle);
 
-	        getTranslatedFieldSupport().renderContributions(this, writer, cycle);
-	        getValidatableFieldSupport().renderContributions(this, writer, cycle);
+			renderDelegateAttributes(writer, cycle);
 
-	        renderInformalParameters(writer, cycle);
-	        
-	        writer.closeTag();
+			getTranslatedFieldSupport()
+					.renderContributions(this, writer, cycle);
+			getValidatableFieldSupport().renderContributions(this, writer,
+					cycle);
 
-	        renderDelegateSuffix(writer, cycle);
-		} else{
-			if(defaultValue==null || defaultValue.toString().trim().length()<1){
+			renderInformalParameters(writer, cycle);
+
+			writer.closeTag();
+
+			renderDelegateSuffix(writer, cycle);
+		} else {
+			if (defaultValue == null
+					|| defaultValue.toString().trim().length() < 1) {
 				super.renderFormComponent(writer, cycle);
-			} else{
-				String value = getTranslatedFieldSupport().format(this, getValue());
-		        if(value != null && value.trim().length()>0){
-		        	super.renderFormComponent(writer, cycle);
-		        }
-		        else{
-		        	this.setValue(this.getDefaultValue());
-		        	super.renderFormComponent(writer, cycle);
-		        }
+			} else {
+				String value = getTranslatedFieldSupport().format(this,
+						getValue());
+				if (value != null && value.trim().length() > 0) {
+					super.renderFormComponent(writer, cycle);
+				} else {
+					this.setValue(this.getDefaultValue());
+					super.renderFormComponent(writer, cycle);
+				}
 			}
 		}
 	}
 
 	/**
 	 * 取得默认值
+	 * 
 	 * @return
 	 */
 	public abstract Object getDefaultValue();
-	
+
 	/**
-	 * 取得该TextField的属性
-	 * true:该TextField为readonly;false:该TextField不是readonly
+	 * 取得该TextField的属性 true:该TextField为readonly;false:该TextField不是readonly
+	 * 
 	 * @return
 	 */
 	public abstract Boolean getOnlyRead();
