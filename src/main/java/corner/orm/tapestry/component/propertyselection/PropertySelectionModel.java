a67707b36095baa1adefa541683c34b3cc8111ab
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-07-26 07:18:52 +0000

    增加了构造方法
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3177 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/propertyselection/PropertySelectionModel.java
+++ src/main/java/corner/orm/tapestry/component/propertyselection/PropertySelectionModel.java
@@ -12,16 +12,35 @@
 
 package corner.orm.tapestry.component.propertyselection;
 
+import java.util.List;
+
+import org.apache.poi.hssf.record.formula.functions.T;
 import org.apache.tapestry.form.IPropertySelectionModel;
 
 /**
  * 进行选择的model，只要传进来数组就可以了
+ * 
  * @author <a href=mailto:xf@bjmaxinfo.com>xiafei</a>
  * @version $Revision$
  * @since 2.3.7
  */
-public class PropertySelectionModel <T> implements
-		IPropertySelectionModel {
+public class PropertySelectionModel<T> implements IPropertySelectionModel {
+	/**
+	 * 默认构造方法
+	 * 
+	 */
+	public PropertySelectionModel() {
+
+	}
+
+	/**
+	 * 直接传入List的构造方法
+	 * 
+	 * @param list
+	 */
+	public PropertySelectionModel(List<T> list) {
+
+	}
 
 	private T[] list;
 
