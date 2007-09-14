f8a8100ddfc06ab6d5ea561eeeb7378c1edee20b
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-09-14 09:41:07 +0000

    增加eventListener测试
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3391 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/demo/page/one/OnePageWithBlob.java
+++ src/test/java/corner/demo/page/one/OnePageWithBlob.java
@@ -12,8 +12,14 @@
 
 package corner.demo.page.one;
 
+import org.apache.tapestry.annotations.EventListener;
+import org.apache.tapestry.annotations.Persist;
+import org.apache.tapestry.event.BrowserEvent;
+import org.apache.tapestry.form.IPropertySelectionModel;
+
 import corner.demo.model.one.A;
 import corner.orm.hibernate.v3.MatrixRow;
+import corner.orm.tapestry.component.propertyselection.PropertySelectionModel;
 import corner.orm.tapestry.page.AbstractEntityFormPage;
 import corner.orm.tapestry.service.blob.IBlobPageDelegate;
 import corner.orm.tapestry.service.blob.SqueezeBlobPageDelegate;
@@ -27,8 +33,24 @@ import corner.orm.tapestry.service.blob.SqueezeBlobPageDelegate;
  */
 public abstract class OnePageWithBlob extends AbstractEntityFormPage<A> {
 
+	@Persist("client")
+	public abstract String getSheetCateType();
 	
+	@EventListener(events="onchange", targets={ "sheetCateHidden" }, elements = { "SheetCateField" })
+	public void onTriggered( BrowserEvent event )
+	{
+		System.out.println(getSheetCateType());
+		event.getMethodArguments();
+		
+//	   System.out.println(event.getMethodArguments().getJSONObject(0).getInt("theAnswer"));
+	}
 	
+	/**
+	 * @return
+	 */
+	public IPropertySelectionModel getSheetCatePageModel() {
+		return new PropertySelectionModel("1,2,3,4,5".split(","));
+	}
 	
 	/**
 	 * @see corner.orm.tapestry.page.AbstractEntityPage#saveOrUpdateEntity()
