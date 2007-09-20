cb42a242b43f235d345257b8f14d804a92756379
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-09-20 08:45:35 +0000

    初步加入了guice对象调用
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3417 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/tapestry/worker/InjectGuice.java
@@ -0,0 +1,39 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.tapestry.worker;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 对guice中的对象进行Inject
+ * 
+ * @author <a href="mailto:jun.tsai@bjmaxinfo.com">Jun Tsai</a>
+ * @version $Revision$
+ * @since 2.3.7
+ */
+@Target( { ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface InjectGuice {
+
+	/**
+	 * 得到需要inject的类对象.
+	 * 
+	 * @return 类对象.
+	 */
+	Class<?> value();
+}
---
+++ src/main/java/corner/orm/tapestry/worker/InjectGuiceWorker.java
@@ -0,0 +1,88 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.tapestry.worker;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import org.apache.hivemind.ApplicationRuntimeException;
+import org.apache.hivemind.Resource;
+import org.apache.hivemind.service.MethodSignature;
+import org.apache.tapestry.annotations.AnnotationUtils;
+import org.apache.tapestry.annotations.SecondaryAnnotationWorker;
+import org.apache.tapestry.enhance.EnhanceUtils;
+import org.apache.tapestry.enhance.EnhancementOperation;
+import org.apache.tapestry.spec.IComponentSpecification;
+
+/**
+ * 对InjectGuice进行增强.
+ * @author <a href="mailto:jun.tsai@bjmaxinfo.com">Jun Tsai</a>
+ * @version $Revision$
+ * @since 2.3.7
+ * @see InjectGuice
+ */
+public class InjectGuiceWorker implements SecondaryAnnotationWorker{
+
+	/**
+	 * 
+	 * @see org.apache.tapestry.annotations.SecondaryAnnotationWorker#canEnhance(java.lang.reflect.Method)
+	 */
+	public boolean canEnhance(Method method) {
+		return method.getAnnotation(InjectGuice.class) != null;
+	}
+	/**
+	 * 
+	 * @see org.apache.tapestry.annotations.SecondaryAnnotationWorker#peformEnhancement(org.apache.tapestry.enhance.EnhancementOperation, org.apache.tapestry.spec.IComponentSpecification, java.lang.reflect.Method, org.apache.hivemind.Resource)
+	 */
+	public void peformEnhancement(EnhancementOperation op, IComponentSpecification spec, Method method, Resource classResource) {
+		
+		//TODO
+		/*
+		 * 利用hivemind中contrib对所有的module进行contribution，
+		 * 利用InjectGuice对service中的类进行统一调用.
+		 * 
+		 * 
+		 */
+		
+		InjectGuice io = method.getAnnotation(InjectGuice.class);
+
+        Class<?> object = io.value();
+
+        String propertyName = AnnotationUtils.getPropertyName(method);
+        Class propertyType = op.getPropertyType(propertyName);
+        if (propertyType == null)
+            propertyType = Object.class;
+
+        op.claimReadonlyProperty(propertyName);
+        //TODO 从guice容器中获得对象.
+        Object injectedValue =null;//_provider.obtainValue(objectReference, location);
+
+        if (injectedValue == null)
+            throw new ApplicationRuntimeException("不能从guice中获得对象",spec.getLocation(), null);
+
+        if (!propertyType.isInstance(injectedValue))
+            throw new ApplicationRuntimeException("错误的对象类型,属性类型为:"+propertyType.getName()+" injectValue:"+injectedValue.getClass().getName(),
+                                                  spec.getLocation(), null);
+
+        String fieldName = op.addInjectedField("_$" + propertyName,
+                                               propertyType, injectedValue);
+
+        String methodName = EnhanceUtils.createAccessorMethodName(propertyName);
+
+        op.addMethod(Modifier.PUBLIC, new MethodSignature(propertyType,
+                                                          methodName, null, null), "return " + fieldName + ";", spec.getLocation());
+        
+        
+	}
+
+}
