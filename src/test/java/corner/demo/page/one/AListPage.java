214868afc3efea10cd1367e2fc541d2ec5ed0e55
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-09-21 04:26:58 +0000

    增加jasperDateSource获得集合循环功能
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3421 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/test/java/corner/demo/page/one/AListPage.java
@@ -0,0 +1,24 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.demo.page.one;
+
+import corner.orm.tapestry.page.PoListPage;
+
+/**
+ * @author <a href=mailto:xf@bjmaxinfo.com>xiafei</a>
+ * @version $Revision$
+ * @since 2.3.7
+ */
+public abstract class AListPage extends PoListPage{
+	
+}
