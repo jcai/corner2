37631a4709cf9bb7a3ca82145dd7eb45cfadd0e0
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-09-24 01:30:29 +0000

    增加jasper支持
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3430 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/demo/page/one/AListPage.java
+++ src/test/java/corner/demo/page/one/AListPage.java
@@ -12,6 +12,19 @@
 
 package corner.demo.page.one;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import org.apache.tapestry.IAsset;
+import org.apache.tapestry.annotations.Asset;
+
+import corner.demo.model.one.A;
 import corner.orm.tapestry.page.PoListPage;
 
 /**
@@ -21,4 +34,72 @@ import corner.orm.tapestry.page.PoListPage;
  */
 public abstract class AListPage extends PoListPage{
 	
+	
+	@Asset("classpath:/jasper/Atest.jasper")
+	public abstract IAsset getJasperAsset();
+	
+	@Asset("classpath:/jasper/Btest.jasper")
+	public abstract IAsset getJasperBAsset();
+	
+	/**
+	 * @return
+	 */
+	public InputStream getReportStream(){
+		return this.getJasperAsset().getResourceAsStream();
+	}
+	
+	/**
+	 * @return
+	 * @throws JRException
+	 */
+	public JasperPrint getJasperPrint() throws JRException{
+		List bowlerInfo = getCollection();
+		return JasperFillManager.fillReport(getJasperBAsset().getResourceAsStream(), null, new JRBeanCollectionDataSource(bowlerInfo));
+	}
+	
+	private List getCollection() {
+		List<Object> rs = new ArrayList<Object>();
+		A c = new A();
+		c.setName("beijing");
+		
+		rs.add(c);
+		return rs;
+	}
+	
+	/**
+	 * @return
+	 */
+	public abstract A getAtest();
+	
+	/**
+	 * 测试使用的
+	 */
+	public List getCollectionTest() {
+		List<Object> rs = new ArrayList<Object>();
+		A c = new A();
+		c.setName("a");
+		rs.add(c);
+		
+		c = new A();
+		c.setName("b");
+		rs.add(c);
+		
+		c = new A();
+		c.setName("c");
+		rs.add(c);
+		
+		c = new A();
+		c.setName("d");
+		rs.add(c);
+		
+		c = new A();
+		c.setName("e");
+		rs.add(c);
+		
+		c = new A();
+		c.setName("f");
+		rs.add(c);
+		
+		return rs;
+	}
 }
