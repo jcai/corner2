8c82269ff73b5bd2aa57aa7b977ad700f13d8dd5
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-09-28 05:03:27 +0000

    增加unzip流操作
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3442 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/util/BaseModelTestCase.java
@@ -0,0 +1,170 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.util;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.support.lob.LobHandler;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import corner.orm.spring.SpringContainer;
+
+/**
+ * @author <a href=mailto:xf@bjmaxinfo.com>xiafei</a>
+ * @version $Revision$
+ * @since 2.3.7
+ */
+public class BaseModelTestCase extends Assert {
+	/**
+	 * Logger for this class
+	 */
+	private static final Log logger = LogFactory
+			.getLog(BaseModelTestCase.class);
+
+	protected org.hibernate.classic.Session session;
+
+	private long end;
+
+	private long star;
+
+	private HibernateTransactionManager transactionManager;
+
+	// 默认的事物定义
+	private TransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+
+	private TransactionStatus status;
+
+	@BeforeMethod(groups = "model")
+	public void initSessionFactory() {
+		star = System.currentTimeMillis();
+		if (transactionManager == null) {
+			initSpring();
+		}
+		status = transactionManager.getTransaction(transactionDefinition);
+		session = transactionManager.getSessionFactory().getCurrentSession();
+
+	}
+
+	@AfterMethod(groups = "model")
+	public void closeSessionFactory() {
+		transactionManager.commit(status);
+		end = System.currentTimeMillis();
+		logger.info("共耗时：" + (end - star) + "毫秒"); //$NON-NLS-1$
+	}
+
+	protected <T> T saveOrUpdate(T obj) {
+		session.saveOrUpdate(obj);
+		return obj;
+
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> T load(Class<T> clazz, Serializable key) {
+		return (T) session.load(clazz, key);
+	}
+
+	/**
+	 * 清空hibernate的缓存。
+	 * 
+	 */
+	protected void flushAndClearSession() {
+		session.flush();
+		session.clear();
+	}
+
+	/**
+	 * 得到映射的文件目录
+	 * 
+	 * @return 文件映射文件目录.
+	 */
+	protected String[] getMappingDirectoryLocations() {
+		return new String[] {};
+	}
+
+	protected String[] getMappingResources() {
+		return null;
+	}
+
+	private Resource[] getMappingDirectoryResources() {
+		if (this.getMappingDirectoryLocations() == null) {
+			return new Resource[0];
+		}
+		List<Resource> list = new ArrayList<Resource>();
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		for (int i = 0; i < getMappingDirectoryLocations().length; i++) {
+			list.add(resolver
+					.getResource(this.getMappingDirectoryLocations()[i]));
+
+		}
+		return list.toArray(new Resource[0]);
+	}
+
+	private void initSpring() {
+		//xf 机器已经更新，采用完全模式进行测试
+		//if ("true".equals(System.getProperty("test.single"))) {
+		if(2>1){
+			transactionManager = (HibernateTransactionManager) SpringContainer.getInstance()
+					.getApplicationContext().getBean("transactionManager");
+
+		} else {
+			SpringContainer container = SpringContainer
+					.getInstance("classpath:/config/spring/application-database.xml");
+
+			AnnotationSessionFactoryBean factoryBean = new org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean();
+			factoryBean.setDataSource((DataSource) container
+					.getApplicationContext().getBean("dataSource"));
+			Properties ps = new Properties();
+			ps.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+			ps.put("hibernate.cache.use_second_level_cache", "false");
+			factoryBean.setHibernateProperties(ps);
+			factoryBean.setConfigLocation(new ClassPathResource(
+					"hibernate.cfg.xml"));
+			factoryBean
+					.setMappingDirectoryLocations(getMappingDirectoryResources());
+			factoryBean.setLobHandler((LobHandler) container
+					.getApplicationContext().getBean("defaultLobHandler"));
+			if (getMappingResources() != null) {
+				factoryBean.setMappingResources(getMappingResources());
+			}
+			try {
+				factoryBean.afterPropertiesSet();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+
+			SessionFactory factory = (SessionFactory) factoryBean.getObject();
+
+			transactionManager = new org.springframework.orm.hibernate3.HibernateTransactionManager();
+			transactionManager.setSessionFactory(factory);
+		}
+	}
+
+}
--- src/test/java/corner/orm/tapestry/jasper/UnZip.java
+++ src/test/java/corner/orm/tapestry/jasper/UnZip.java
@@ -14,32 +14,112 @@ package corner.orm.tapestry.jasper;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import corner.demo.model.one.A;
+import corner.util.BaseModelTestCase;
 
 /**
  * @author <a href=mailto:xf@bjmaxinfo.com>xiafei</a>
  * @version $Revision$
  * @since 2.3.7
  */
-public class UnZip extends Assert{
+public class UnZip extends BaseModelTestCase{
+	
+	//解压缩的文件
+	String fileName = System.getProperty("user.dir") + "\\src\\test\\resources\\jasper\\Test.zip";
+	
+	//获得系统的临时目录路径
+	String tmpdir = System.getProperty("java.io.tmpdir");
+	
+	/**
+	 * @see com.bjmaxinfo.piano.model.BaseModelTestCase#getMappingDirectoryLocations()
+	 */
+	@Override
+	protected String[] getMappingDirectoryLocations() {
+		return new String[]{"classpath:/corner/demo/model/one"
+							};
+	}
+	
 	static final int BUFFER = 2048;
+	
+	/**
+	 * 
+	 */
+	@Test(groups="model")
+	public void testUnZipBlob(){
+		A main = null;
+//		main.setName("a");
+//		
+//		this.saveOrUpdate(main);
+//		
+//		this.flushAndClearSession();
+		main = (A) this.load(A.class, "282828c1154a53a501154a54c4780001");
 
-	@Test
-	public void testUnZip() {
+		InputStream bos = new ByteArrayInputStream(main.getBlobData());
+
+		ZipInputStream zis = null;
+		zis = new ZipInputStream(bos);
+		BufferedOutputStream dest = null;
+		ZipEntry entry = null;
 		try {
-			//解压缩的文件
-			String fileName = System.getProperty("user.dir") + "\\src\\test\\resources\\jasper\\Test.zip";
-			
-			//获得系统的临时目录路径
-			String tmpdir = System.getProperty("java.io.tmpdir");
-			ZipFile zipFile = new ZipFile(fileName);
+			while ((entry = zis.getNextEntry()) != null) {
+				if (entry.isDirectory()) {
+					new File(tmpdir + entry.getName()).mkdirs();
+					continue;
+				}
+				
+				System.out.println("Extracting: " + entry);
+				int count;
+				byte data[] = new byte[BUFFER];
+
+				File file = new File(tmpdir + entry.getName());
+				
+				// write the files to the disk
+				FileOutputStream fos = new FileOutputStream(file);
+				dest = new BufferedOutputStream(fos, BUFFER);
+				while ((count = zis.read(data, 0, BUFFER)) != -1) {
+					dest.write(data, 0, count);
+				}
+				dest.flush();
+				dest.close();
+			}
+			zis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 通过文件测试
+	 */
+//	@Test
+	public void testUnZip() {
+		ZipFile zipFile = null;
+		try {
+			zipFile = new ZipFile(fileName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.UnZipAction(zipFile);
+	}
+
+	/**
+	 * 
+	 * 处理需要解压缩的文件
+	 */
+	private void UnZipAction(ZipFile zipFile) {
+		try {
 			Enumeration emu = zipFile.entries();
 			
 			while (emu.hasMoreElements()) {
