dc7af5e300e0caeb3c9755edeeee2d1306a8c95c
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-09-29 08:23:29 +0000

    使用cornerpdfexporter
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3472 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/jasper/exporter/JPdfExporter.java
+++ src/main/java/corner/orm/tapestry/jasper/exporter/JPdfExporter.java
@@ -8,7 +8,6 @@
 package corner.orm.tapestry.jasper.exporter;
 
 import net.sf.jasperreports.engine.JRExporter;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
 
 /**
  * @author <a href=mailto:xf@bjmaxinfo.com>xiafei</a>
@@ -21,7 +20,7 @@ public class JPdfExporter extends AbstractJasperExporter{
 	 * @see poison.preplan.tapestry.jasper.exporter.AbstractJasperExporter#getRExporter()
 	 */
 	public JRExporter getExporter() {
-		return new JRPdfExporter();
+		return new CornerPdfExporter();
 	}
 
 	/**
