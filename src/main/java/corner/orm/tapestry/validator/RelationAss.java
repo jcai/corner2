33efd6980a2e9f5f20baa518390620e7cd0cfafd
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-10-15 08:33:04 +0000

    暂时删除js判断
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3501 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/validator/RelationAss.java
+++ src/main/java/corner/orm/tapestry/validator/RelationAss.java
@@ -12,17 +12,10 @@
 
 package corner.orm.tapestry.validator;
 
-import java.text.DecimalFormatSymbols;
-
-import org.apache.tapestry.IMarkupWriter;
 import org.apache.tapestry.IRequestCycle;
-import org.apache.tapestry.form.FormComponentContributorContext;
 import org.apache.tapestry.form.IFormComponent;
 import org.apache.tapestry.form.ValidationMessages;
 import org.apache.tapestry.form.validator.BaseValidator;
-import org.apache.tapestry.json.JSONLiteral;
-import org.apache.tapestry.json.JSONObject;
-import org.apache.tapestry.valid.ValidationConstants;
 import org.apache.tapestry.valid.ValidatorException;
 
 /**
@@ -54,39 +47,39 @@ public class RelationAss extends BaseValidator{
 			throw new ValidatorException(buildMessage(messages, field));
 	}
 	
-	/**
-	 * @see org.apache.tapestry.form.validator.BaseValidator#renderContribution(org.apache.tapestry.IMarkupWriter,
-	 *      org.apache.tapestry.IRequestCycle,
-	 *      org.apache.tapestry.form.FormComponentContributorContext,
-	 *      org.apache.tapestry.form.IFormComponent)
-	 */
-	@Override
-	public void renderContribution(IMarkupWriter writer, IRequestCycle cycle,
-			FormComponentContributorContext context, IFormComponent field) {
-
-		context.addInitializationScript(field,
-				"dojo.require(\"corner.validate.web\");");
-
-		JSONObject profile = context.getProfile();
-
-		if (!profile.has(ValidationConstants.CONSTRAINTS)) {
-			profile.put(ValidationConstants.CONSTRAINTS, new JSONObject());
-		}
-		JSONObject cons = profile
-				.getJSONObject(ValidationConstants.CONSTRAINTS);
-
-		DecimalFormatSymbols symbols = new DecimalFormatSymbols(context
-				.getLocale());
-
-		accumulateProperty(cons, field.getClientId(), new JSONLiteral(
-				"[corner.validate.isRelationAss,{" + "\"fields\":["
-						+ field.getClientId() + ASSOCIATE_SUFFIX + "]," + "decimal:"
-						+ JSONObject.quote(symbols.getDecimalSeparator())
-						+ "}]"));
-
-		accumulateProfileProperty(field, profile,
-				ValidationConstants.CONSTRAINTS, buildMessage(context,field));
-	}
+//	/**
+//	 * @see org.apache.tapestry.form.validator.BaseValidator#renderContribution(org.apache.tapestry.IMarkupWriter,
+//	 *      org.apache.tapestry.IRequestCycle,
+//	 *      org.apache.tapestry.form.FormComponentContributorContext,
+//	 *      org.apache.tapestry.form.IFormComponent)
+//	 */
+//	@Override
+//	public void renderContribution(IMarkupWriter writer, IRequestCycle cycle,
+//			FormComponentContributorContext context, IFormComponent field) {
+//
+//		context.addInitializationScript(field,
+//				"dojo.require(\"corner.validate.web\");");
+//
+//		JSONObject profile = context.getProfile();
+//
+//		if (!profile.has(ValidationConstants.CONSTRAINTS)) {
+//			profile.put(ValidationConstants.CONSTRAINTS, new JSONObject());
+//		}
+//		JSONObject cons = profile
+//				.getJSONObject(ValidationConstants.CONSTRAINTS);
+//
+//		DecimalFormatSymbols symbols = new DecimalFormatSymbols(context
+//				.getLocale());
+//
+//		accumulateProperty(cons, field.getClientId(), new JSONLiteral(
+//				"[corner.validate.isRelationAss,{" + "\"fields\":["
+//						+ field.getClientId() + ASSOCIATE_SUFFIX + "]," + "decimal:"
+//						+ JSONObject.quote(symbols.getDecimalSeparator())
+//						+ "}]"));
+//
+//		accumulateProfileProperty(field, profile,
+//				ValidationConstants.CONSTRAINTS, buildMessage(context,field));
+//	}
 
 	/**
 	 * 构建message
