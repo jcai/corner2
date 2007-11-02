ddd72314adacde4587889924c475ad666243e74a
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-11-02 02:42:11 +0000

    移动IPersitModel到corner中，便于进行抽象以及重构
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3578 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/hibernate/IPersistModel.java
@@ -0,0 +1,23 @@
+/*		
+ * Copyright 2006-2007 The Beijing Maxinfo Technology Ltd. 
+ * site: http://www.bjmaxinfo.com
+ * file: $Id$
+ * created at:2006-12-21
+ */
+
+package corner.orm.hibernate;
+
+/**
+ * 所有实现持久化的model接口.
+ * @author <a href="mailto:jun.tsai@bjmaxinfo.com">Jun Tsai</a>
+ * @version $Revision$
+ * @since 2.5
+ */
+public interface IPersistModel {
+	/**
+	 * 返回实体的主键
+	 * @return Returns the id.
+	 */
+	public abstract String getId();
+
+}
