3d2d6736832db5dd8a8d09b0d86b3f9588ae7e1c
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-11-12 08:28:05 +0000

    使用新的js设置svn颜色
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3653 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
+++ src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
@@ -1,20 +1,21 @@
 /*  
  * 显示字体
  */
-function showText(clientId,json,jsonOther,entityName,showProperty){
+function showText(clientId,json,jsonOther,entityName,showProperty,type){
 //	dojo.debug("ClientId： " + clientId + " Json: " + json + " JsonOther: " + jsonOther + " EntityName: " + entityName + " ShowProperty: " + showProperty);
-	
-	var ver = showVerNum("otherVer_hid");
 	
 	var date1 = json[entityName][showProperty];
 	var date2 = jsonOther[entityName][showProperty];
 	
-	if(date1!=null && date2==null){	//删除
-		$(clientId).update("<font color='#88f'> "+ date1 + "</font>");
-	}else{	//改、增
+	dojo.debug("type: " + type);
+	
+	if(type == "show"){
+		if(date1 != null){
+		$(clientId).update(date1);
+	}
+	}else{
 		showFieldText(clientId,date1,date2);
-		new Tip($(clientId), date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
-	} 
+	}
 }
 
 /*  
@@ -22,14 +23,26 @@ function showText(clientId,json,jsonOther,entityName,showProperty){
  * 未记入svn的用#bfb表示，改变的用#fd8表示
  */
 function showFieldText(clientId,date1,date2){
-	
 	dojo.debug("date1: " + date1);
 	dojo.debug("date2: " + date2);
 	
-	if(date1 == null && date2 != null){
-		$(clientId).update("<font color='#bfb'> "+ $(clientId).innerHTML + "</font>");
-	}else{
-		$(clientId).update("<font color='#fd8'> "+ date1 + "</font>");
+	var ver = showVerNum("otherVer_hid");
+	
+	if(date1!=null && date2==null){	//增加
+		$(clientId).update("<font color='#bfb'> "+ date1 + "</font>");
+	}
+	
+	if(date1==null && date2!=null){	//删除
+		$(clientId).update("<font color='#88f'> "+ date1 + "</font>");
+	}
+	
+	if(date1!=null && date2!=null && date1 != date2){	//修改
+		$(clientId).update("<font color='#88f'> "+ date1 + "</font>");
+		new Tip($(clientId), date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
+	}
+	
+	if(date1!=null && date2!=null && date1 == date2){	//显示
+		dojo.debug("show");
 	}
 }
 
