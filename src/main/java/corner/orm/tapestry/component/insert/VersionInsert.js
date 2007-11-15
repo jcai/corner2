5ae1323810b52022d92c73f26fdcfe3a495ac978
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-11-15 09:44:53 +0000

    增加隐藏全部tip和展示全部tip的功能
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3691 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
+++ src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
@@ -6,21 +6,16 @@ var VersionInserts = {
 	add: function(versionInsert) {
 		this.versionInserts.push(versionInsert);
 	},
-
-	remove: function(element) {
-		var versionInsert = this.versionInserts.find(function(t){ return t.element == $(element); });
-		if (!versionInsert) return;
-
-		this.versionInserts = this.versionInserts.reject(function(t) { return t==versionInsert; });
-		if(versionInsert.toolversionInsert) versionInsert.wrapper.remove();
-		if(versionInsert.underlay) versionInsert.underlay.remove();
+	resetTips: function(){	//全部显示
+		dojo.debug("run resetTips.");
+		for(i=0;i<this.versionInserts.length;i++){
+			this.versionInserts[i].tip.activate();
+		}
 	},
-	reSetVI: function(){
-		for(var key in this.versionInserts){
-			dojo.debug("key : " + key);
-			dojo.debug("versionInserts[key] : " + this.versionInserts[key]);
-			var versionInsert = this.versionInserts[key];
-			versionInsert.setFieldText();
+	closeTips: function(){	//全部隐藏
+		dojo.debug("run closeTips.");
+		for(i=0;i<this.versionInserts.length;i++){
+			this.versionInserts[i].tip.deactivate();
 		}
 	}
 }
@@ -31,18 +26,19 @@ var VersionInserts = {
 var VersionInsert = Class.create();
 VersionInsert.prototype = {
 	initialize: function(element,json,jsonOther,entityName,showProperty,type) {
-		this.element = $(element);
-		VersionInserts.remove(this.element);
+		this.element = element;
+		
+		
 		this.entityName = entityName;
 		this.showProperty = showProperty;
 		this.type = type;
 		this.date1 = json[entityName][showProperty];
 		this.date2 = jsonOther[entityName][showProperty];
+		this.tip = null;
 		
 		this.setFieldText();	//调用设置方法
 		
 		VersionInserts.add(this);
-		//VersionInserts.reSetVI();
 	},
 	setFieldText : function(){
 		dojo.debug("type: " + this.type);
@@ -66,30 +62,30 @@ VersionInsert.prototype = {
 		var ver = this.showVerNum("otherVer_hid");
 		
 		if(this.date1!=null && this.date2==null){	//删除
-			this.element.setStyle({
+			$(this.element).setStyle({
 			backgroundColor: '#FF3E3E',
 			fontSize: '12px',
 				'text-decoration': 'line-through',
 				color:'white'
 			});
-			this.element.update(this.date1);
+			$(this.element).update(this.date1);
 		}
 		
 		if(this.date1==null && this.date2!=null){	//增加
-			this.element.setStyle({
+			$(this.element).setStyle({
 			backgroundColor: '#bfb',
 			'line-height':'150%'
 			});
-			this.element.update("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			new Tip(this.element, this.date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
+			$(this.element).update("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+			this.tip = new Tip($(this.element), this.date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
 		}
 		
 		if(this.date1!=null && this.date2!=null && this.date1 != this.date2){	//修改
-			this.element.setStyle({
+			$(this.element).setStyle({
 				backgroundColor: '#fd8'
 				});
-			this.element.update(this.date1);
-			new Tip(this.element, this.date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
+			$(this.element).update(this.date1);
+			this.tip = new Tip($(this.element), this.date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
 		}
 		
 		if(this.date1!=null && this.date2!=null && this.date1 == this.date2){	//显示
