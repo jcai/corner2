2cbf2ca9b4c0015a7063cd700aa545cb1b9ec6ba
jcai <jcai@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-11-15 02:37:29 +0000

    调整修改时候的css展示
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3688 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
+++ src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
@@ -4,17 +4,17 @@
 function showText(clientId,json,jsonOther,entityName,showProperty,type){
 //	dojo.debug("ClientId： " + clientId + " Json: " + json + " JsonOther: " + jsonOther + " EntityName: " + entityName + " ShowProperty: " + showProperty);
 	
-	var date1 = json[entityName][showProperty];
-	var date2 = jsonOther[entityName][showProperty];
+	var data1 = json[entityName][showProperty];
+	var data2 = jsonOther[entityName][showProperty];
 	
 	dojo.debug("type: " + type);
 	
 	if(type == "show"){
-		if(date1 != null){
-		$(clientId).update(date1);
+		if(data1 != null){
+		$(clientId).update(data1);
 	}
 	}else{
-		showFieldText(clientId,date1,date2);
+		showFieldText(clientId,data1,data2);
 	}
 }
 
@@ -22,37 +22,40 @@ function showText(clientId,json,jsonOther,entityName,showProperty,type){
  * 显示更新显示文字
  * 未记入svn的用#bfb表示，改变的用#fd8表示
  */
-function showFieldText(clientId,date1,date2){
-	dojo.debug("date1: " + date1);
-	dojo.debug("date2: " + date2);
+function showFieldText(clientId,data1,data2){
+	dojo.debug("data1: " + data1);
+	dojo.debug("data2: " + data2);
 	
 	var ver = showVerNum("otherVer_hid");
 	
-	if(date1!=null && date2==null){	//删除
+	if(data1!=null && data2==null){	//删除
 		$(clientId).setStyle({
 		  backgroundColor: '#FF3E3E',
 		  fontSize: '12px',
 			'text-decoration': 'line-through',
 			color:'white'
 		});
-		$(clientId).update(date1);
+		$(clientId).update(data1);
 	}
 	
-	if(date1==null && date2!=null){	//增加
+	if(data1==null && data2!=null){	//增加
 		$(clientId).setStyle({
 		  backgroundColor: '#bfb',
 		  'line-height':'150%'
 		});
 		$(clientId).update("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-		new Tip($(clientId), date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
+		new Tip($(clientId), data2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
 	}
 	
-	if(date1!=null && date2!=null && date1 != date2){	//修改
-		$(clientId).update("<font color='#fd8'>"+ date1 + "</font>");
-		new Tip($(clientId), date2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
+	if(data1!=null && data2!=null && data1 != data2){	//修改
+		$(clientId).setStyle({
+			  backgroundColor: '#fd8'
+			});
+		$(clientId).update(data1);
+		new Tip($(clientId), data2,{footer:ver,fixed:true,hook:{target:'topLeft',tip:'bottomLeft'}});
 	}
 	
-	if(date1!=null && date2!=null && date1 == date2){	//显示
+	if(data1!=null && data2!=null && data1 == data2){	//显示
 		dojo.debug("show");
 	}
 }
