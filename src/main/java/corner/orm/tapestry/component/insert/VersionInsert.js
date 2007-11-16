0c4cacaac868a144f9a30eb024ee7a58cab12fd8
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-11-16 02:03:25 +0000

    写完xinDiff的demo
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3692 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
+++ src/main/java/corner/orm/tapestry/component/insert/VersionInsert.js
@@ -9,13 +9,17 @@ var VersionInserts = {
 	resetTips: function(){	//全部显示
 		dojo.debug("run resetTips.");
 		for(i=0;i<this.versionInserts.length;i++){
-			this.versionInserts[i].tip.activate();
+			$(this.versionInserts[i].element).setStyle({
+				display: ''
+			});
 		}
 	},
 	closeTips: function(){	//全部隐藏
 		dojo.debug("run closeTips.");
 		for(i=0;i<this.versionInserts.length;i++){
-			this.versionInserts[i].tip.deactivate();
+			$(this.versionInserts[i].element).setStyle({
+				display: 'none'
+			});
 		}
 	}
 }
@@ -27,18 +31,23 @@ var VersionInsert = Class.create();
 VersionInsert.prototype = {
 	initialize: function(element,json,jsonOther,entityName,showProperty,type) {
 		this.element = element;
-		
-		
 		this.entityName = entityName;
 		this.showProperty = showProperty;
 		this.type = type;
 		this.date1 = json[entityName][showProperty];
 		this.date2 = jsonOther[entityName][showProperty];
 		this.tip = null;
+		this.diffPostfix = "_diff";
+		this.diffElement = element + this.diffPostfix;
 		
 		this.setFieldText();	//调用设置方法
 		
+		this.setDiffFieldText();
+		
 		VersionInserts.add(this);
+	},
+	setDiffFieldText:function(){
+		this.diffBy();
 	},
 	setFieldText : function(){
 		dojo.debug("type: " + this.type);
@@ -102,5 +111,169 @@ VersionInsert.prototype = {
 		}else{
 			return "MAXINFO";
 		}
+	},
+	/*
+	 下面是XinDiff的调用
+	 */
+	createMap: function (fast) {
+	    return document.all ? new ActiveXObject('Scripting.Dictionary') : new Dictionary(fast);
+	},
+	doDiff: function (left, right) {
+	    var diff = new XinDiffEngine(this.createMap);
+	    diff.assign(left.words, 1);
+	    diff.assign(right.words, 0);
+	
+	    var output = new XinDiffOutput(this.createMap);
+	    output.diff = diff;
+	    output.symbols = [right.symbols, left.symbols];
+	
+	    diff.doDiff().serialize(diff, output);
+	    $(this.diffElement).innerHTML = output.getHTML();
+	},
+	diffBy: function () {
+	    this.doDiff(this.splitWords(this.date1), this.splitWords(this.date2));
+	},
+	splitCharacters: function (text) {
+	    var chars = [];
+	    var blanks = this.createMap(true);
+	    var blank = '';
+	    var length = text.length;
+	    for (var i = 0; i < length; ++i) {
+	        var ch = text.charAt(i);
+	        switch (ch) {
+	        case ' ':
+	        case '\n':
+	        case '\r':
+	        case '\t':
+	            blank += ch;
+	            break;
+	        default:
+	            if (blank != '') {
+	                blanks.Add(chars.length, blank);
+	                blank = '';
+	            }
+	            chars.push(ch);
+	            break;
+	        }
+	    }
+	    if (blank != '') {
+	        blanks.Add(chars.length, blank);
+	    }
+	    return {'words': chars, 'symbols': blanks};
+	},
+	splitLines: function (text) {
+	    var lines = [];
+	    var blanks = createMap(true);
+	    var line = '';
+	    var blank = '';
+	    var length = text.length;
+	    for (var i = 0; i < length; ++i) {
+	        var ch = text.charAt(i);
+	        switch (ch) {
+	        case '\n':
+	        case '\r':
+	        //case ' ':
+	        //case '\t':
+	            if (line != '') {
+	                lines.push(line);
+	                line = '';
+	            }
+	            blank += ch;
+	            break;
+	        default:
+	            if (blank != '') {
+	                blanks.Add(lines.length, blank);
+	                blank = '';
+	            }
+	            line += ch;
+	            break;
+	        }
+	    }
+	    if (line != '') {
+	        lines.push(line);
+	    }
+	    else if (blank != '') {
+	        blanks.Add(lines.length, blank);
+	    }
+	    return {'words': lines, 'symbols': blanks};
+	},
+	isLiteral: function (ch) {
+	    return ((ch <= 'z') && (ch >= 'a')) || ((ch <= 'Z') && (ch >= 'A')) ||
+	        ((ch <= '9') && (ch >= '0')) || (ch == '_') || (ch == '-') || (ch == '+');
+	},
+	splitWords: function (text) {
+	    var words = [];
+	    var symbols = this.createMap(true);
+	    var length = text.length;
+	    var word = '';
+	    var symbol = '';
+	    for (var i = 0; i < length; ++i) {
+	        if (text.charCodeAt(i) >= 256) {
+	            if (word != '') {
+	                words.push(word);
+	                word = '';
+	            }
+	            else if (symbol != '') {
+	                symbols.Add(words.length, symbol);
+	                symbol = '';
+	            }
+	            words.push(text.charAt(i));
+	        }
+	        else {
+	            var ch = text.charAt(i);
+	            if (this.isLiteral(ch)) {
+	                if (symbol != '') {
+	                    symbols.Add(words.length, symbol);
+	                    symbol = '';
+	                }
+	                word += ch;
+	            }
+	            else {
+	                if (word != '') {
+	                    words.push(word);
+	                    word = '';
+	                }
+	                symbol += ch;
+	            }
+	        }
+	    }
+	    if (word != '') {
+	        words.push(word);
+	    }
+	    else if (symbol != '') {
+	        symbols.Add(words.length, symbol);
+	    }
+	    return {'words': words, 'symbols': symbols};
+	}
+}
+
+var Dictionary = Class.create();
+Dictionary.prototype = {
+	initialize: function(fast) {
+	    this.map = {};
+	
+	    this.Add = fast ?
+	        function(key, value) {
+	            this.map[key] = value;
+	        }:
+	        function(key, value) {
+	            this.map['_' + key] = value;
+	        };
+	
+	    this.Exists = fast ?
+	        function(key) {
+	            return this.map.hasOwnProperty(key);
+	        }:
+	        function(key) {
+	            return this.map.hasOwnProperty('_' + key);
+	        };
+	
+	    this.Item = fast ?
+	        function(key) {
+	            return this.map[key];
+	        }:
+	        function(key) {
+	            return this.map['_' + key];
+	        };
 	}
 }
