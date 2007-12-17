23a90727b83958c4005e73da27997a06f9913647
jetty <jetty@bda6c913-8cf9-0310-89cc-b5856a464ced>
2007-12-17 06:16:25 +0000

    增加输出流测试
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3792 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/service/svn/HibernateObjectTest.java
+++ src/test/java/corner/service/svn/HibernateObjectTest.java
@@ -12,10 +12,15 @@
 
 package corner.service.svn;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
 import org.testng.annotations.Test;
+
+import com.thoughtworks.xstream.core.util.Base64Encoder;
 
 import corner.test.AbstractTestCase;
 
@@ -26,9 +31,32 @@ import corner.test.AbstractTestCase;
  */
 public class HibernateObjectTest extends AbstractTestCase{
 	private static final Log logger = LogFactory.getLog(HibernateObjectTest.class);
+	private static final Base64Encoder base64 = new Base64Encoder();
 	
 	@Test
-	public void testBase64(){
+	public void testDecodeBase64(){
+		byte[] bytes = base64.decode("dGVzdA==");
+		
+		ByteArrayInputStream input = new ByteArrayInputStream(bytes);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		int ch = 0;
+		
+		try{
+            while((ch = input.read()) != -1){
+                out.write((char)ch);
+            }
+            input.close();
+            out.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        assertEquals("test", new String(out.toByteArray()));
+	}
+	
+	@Test
+	public void testEncodeBase64(){
 		this.startTransaction();
 		Session session=this.getCurrentSession();
 		
@@ -40,7 +68,7 @@ public class HibernateObjectTest extends AbstractTestCase{
 		System.out.println(XStreamDelegate.toJSON(a1));
 	}
 	
-//	@Test
+	@Test
 	public void testA(){
 		logger.debug("----------------------对A的操作--------------");
 		//保存A
