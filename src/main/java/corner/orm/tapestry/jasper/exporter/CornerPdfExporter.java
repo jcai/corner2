7090e1d447827dbd4b265ca89463e7ff6704bb3d
lsq <lsq@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-02-02 09:19:00 +0000

    修改 自动缩小字体的功能.现在是先模拟添加数据到ColumnText判断是否溢出.溢出则用TextField否则用ColumnText
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3933 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/jasper/exporter/CornerPdfExporter.java
+++ src/main/java/corner/orm/tapestry/jasper/exporter/CornerPdfExporter.java
@@ -20,6 +20,7 @@ package corner.orm.tapestry.jasper.exporter;
 import java.awt.Color;
 import java.awt.geom.AffineTransform;
 import java.io.IOException;
+import java.text.AttributedCharacterIterator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,14 +33,19 @@ import net.sf.jasperreports.engine.util.JRStyledText;
 
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.ColumnText;
+import com.lowagie.text.pdf.PdfWriter;
 import com.lowagie.text.pdf.TextField;
 
 import corner.orm.tapestry.pdf.PdfUtils;
 
 /**
  * 替换Pdf中ColumnText为TextField的exporter
- * <p>jasperreport中，导出Pdf文件的时候，默认使用的是ColumnText显示文本,
+ * <p>
+ * jasperreport中，导出Pdf文件的时候，默认使用的是ColumnText显示文本,
  * 使用这个exporter可以替换ColumnText为TextField
  * 
  * @author <a href=mailto:Ghostbb@bjmaxinfo.com>Ghostbb</a>
@@ -48,34 +54,28 @@ import corner.orm.tapestry.pdf.PdfUtils;
  */
 public class CornerPdfExporter extends JRPdfExporter {
 
-	public static final String FIELD_NAME_PREFIX="FIELD";
-	private int i=0;
-	private List<String> alreadyExistFields=new ArrayList<String>();
-	
-	String createUniqueName(){
-		i+=1;
-		return FIELD_NAME_PREFIX+i;
+	public static final String FIELD_NAME_PREFIX = "FIELD";
+	private int i = 0;
+	private List<String> alreadyExistFields = new ArrayList<String>();
+
+	String createUniqueName() {
+		i += 1;
+		return FIELD_NAME_PREFIX + i;
 	}
-	
+
 	/**
-	 * 提供了pdf字体根据框的大小自动缩小的功能.同时以是否有横线(-)为条件判断是否需要自动缩小.
+	 * 重写了此方法.提供判断文字是否溢出从而自动缩小字体的功能.
+	 * <p>判断方法:用ColumnText模拟录入下数据,
+	 * 若返回状态为NO_MORE_COLUMN即数据溢出没有更多的行放数据,此时用TextField来填充数据.
+	 * TextField有自动缩小字体的功能.
 	 * @see net.sf.jasperreports.engine.export.JRPdfExporter#exportText(net.sf.jasperreports.engine.JRPrintText)
 	 */
 	@Override
 	protected void exportText(JRPrintText text) throws DocumentException {
-		
-		//如果不是画了横线 则用父类的方法.
-		if(!text.isStrikeThrough()){
-		   
-			super.exportText(text);
-			
-			return;
-		}
-		
+
 		JRStyledText styledText = getStyledText(text, false);
 
-		if (styledText == null)
-		{
+		if (styledText == null) {
 			return;
 		}
 
@@ -95,191 +95,196 @@ public class CornerPdfExporter extends JRPdfExporter {
 
 		double angle = 0;
 
-		switch (text.getRotation())
-		{
-			case JRTextElement.ROTATION_LEFT :
-			{
-				y = text.getY() + getOffsetY() + text.getHeight();
-				xFillCorrection = 1;
-				width = text.getHeight();
-				height = text.getWidth();
-				int tmpPadding = topPadding;
-				topPadding = leftPadding;
-				leftPadding = bottomPadding;
-				bottomPadding = rightPadding;
-				rightPadding = tmpPadding;
-				angle = Math.PI / 2;
-				break;
-			}
-			case JRTextElement.ROTATION_RIGHT :
-			{
-				x = text.getX() + getOffsetX() + text.getWidth();
-				yFillCorrection = -1;
-				width = text.getHeight();
-				height = text.getWidth();
-				int tmpPadding = topPadding;
-				topPadding = rightPadding;
-				rightPadding = bottomPadding;
-				bottomPadding = leftPadding;
-				leftPadding = tmpPadding;
-				angle = - Math.PI / 2;
-				break;
-			}
-			case JRTextElement.ROTATION_UPSIDE_DOWN :
-			{
-				x = text.getX() + getOffsetX() + text.getWidth();
-				y = text.getY() + getOffsetY() + text.getHeight();
-				int tmpPadding = topPadding;
-				topPadding = bottomPadding;
-				bottomPadding = tmpPadding;
-				tmpPadding = leftPadding;
-				leftPadding = rightPadding;
-				rightPadding = tmpPadding;
-				angle = Math.PI;
-				break;
-			}
-			case JRTextElement.ROTATION_NONE :
-			default :
-			{
-			}
+		switch (text.getRotation()) {
+		case JRTextElement.ROTATION_LEFT: {
+			y = text.getY() + getOffsetY() + text.getHeight();
+			xFillCorrection = 1;
+			width = text.getHeight();
+			height = text.getWidth();
+			int tmpPadding = topPadding;
+			topPadding = leftPadding;
+			leftPadding = bottomPadding;
+			bottomPadding = rightPadding;
+			rightPadding = tmpPadding;
+			angle = Math.PI / 2;
+			break;
+		}
+		case JRTextElement.ROTATION_RIGHT: {
+			x = text.getX() + getOffsetX() + text.getWidth();
+			yFillCorrection = -1;
+			width = text.getHeight();
+			height = text.getWidth();
+			int tmpPadding = topPadding;
+			topPadding = rightPadding;
+			rightPadding = bottomPadding;
+			bottomPadding = leftPadding;
+			leftPadding = tmpPadding;
+			angle = -Math.PI / 2;
+			break;
+		}
+		case JRTextElement.ROTATION_UPSIDE_DOWN: {
+			x = text.getX() + getOffsetX() + text.getWidth();
+			y = text.getY() + getOffsetY() + text.getHeight();
+			int tmpPadding = topPadding;
+			topPadding = bottomPadding;
+			bottomPadding = tmpPadding;
+			tmpPadding = leftPadding;
+			leftPadding = rightPadding;
+			rightPadding = tmpPadding;
+			angle = Math.PI;
+			break;
+		}
+		case JRTextElement.ROTATION_NONE:
+		default: {
+		}
 		}
 
 		AffineTransform atrans = new AffineTransform();
 		atrans.rotate(angle, x, jasperPrint.getPageHeight() - y);
 		pdfContentByte.transform(atrans);
 
-		if (text.getMode() == JRElement.MODE_OPAQUE)
-		{
+		if (text.getMode() == JRElement.MODE_OPAQUE) {
 			Color backcolor = text.getBackcolor();
-			pdfContentByte.setRGBColorStroke(
-				backcolor.getRed(),
-				backcolor.getGreen(),
-				backcolor.getBlue()
-				);
-			pdfContentByte.setRGBColorFill(
-				backcolor.getRed(),
-				backcolor.getGreen(),
-				backcolor.getBlue()
-				);
+			pdfContentByte.setRGBColorStroke(backcolor.getRed(), backcolor
+					.getGreen(), backcolor.getBlue());
+			pdfContentByte.setRGBColorFill(backcolor.getRed(), backcolor
+					.getGreen(), backcolor.getBlue());
 			pdfContentByte.setLineWidth(1f);
 			pdfContentByte.setLineDash(0f);
-			pdfContentByte.rectangle(
-				x + xFillCorrection,
-				jasperPrint.getPageHeight() - y + yFillCorrection,
-				width - 1,
-				- height + 1
-				);
+			pdfContentByte.rectangle(x + xFillCorrection, jasperPrint
+					.getPageHeight()
+					- y + yFillCorrection, width - 1, -height + 1);
 			pdfContentByte.fillStroke();
-		}
-		else
-		{
+		} else {
 			/*
-			pdfContentByte.setRGBColorStroke(
-				text.getForecolor().getRed(),
-				text.getForecolor().getGreen(),
-				text.getForecolor().getBlue()
-				);
-			pdfContentByte.setLineWidth(0.1f);
-			pdfContentByte.setLineDash(0f);
-			pdfContentByte.rectangle(
-				text.getX() + offsetX,
-				jasperPrint.getPageHeight() - text.getY() - offsetY,
-				text.getWidth(),
-				- text.getHeight()
-				);
-			pdfContentByte.stroke();
-			*/
+			 * pdfContentByte.setRGBColorStroke( text.getForecolor().getRed(),
+			 * text.getForecolor().getGreen(), text.getForecolor().getBlue() );
+			 * pdfContentByte.setLineWidth(0.1f);
+			 * pdfContentByte.setLineDash(0f); pdfContentByte.rectangle(
+			 * text.getX() + offsetX, jasperPrint.getPageHeight() - text.getY() -
+			 * offsetY, text.getWidth(), - text.getHeight() );
+			 * pdfContentByte.stroke();
+			 */
 		}
 
-		if (textLength > 0)
-		{
+		if (textLength > 0) {
 			int horizontalAlignment = Element.ALIGN_LEFT;
-			switch (text.getHorizontalAlignment())
-			{
-				case JRAlignment.HORIZONTAL_ALIGN_LEFT :
-				{
-					if (text.getRunDirection() == JRPrintText.RUN_DIRECTION_LTR)
-					{
-						horizontalAlignment = Element.ALIGN_LEFT;
-					}
-					else
-					{
-						horizontalAlignment = Element.ALIGN_RIGHT;
-					}
-					break;
+			switch (text.getHorizontalAlignment()) {
+			case JRAlignment.HORIZONTAL_ALIGN_LEFT: {
+				if (text.getRunDirection() == JRPrintText.RUN_DIRECTION_LTR) {
+					horizontalAlignment = Element.ALIGN_LEFT;
+				} else {
+					horizontalAlignment = Element.ALIGN_RIGHT;
 				}
-				case JRAlignment.HORIZONTAL_ALIGN_CENTER :
-				{
-					horizontalAlignment = Element.ALIGN_CENTER;
-					break;
-				}
-				case JRAlignment.HORIZONTAL_ALIGN_RIGHT :
-				{
-					if (text.getRunDirection() == JRPrintText.RUN_DIRECTION_LTR)
-					{
-						horizontalAlignment = Element.ALIGN_RIGHT;
-					}
-					else
-					{
-						horizontalAlignment = Element.ALIGN_LEFT;
-					}
-					break;
-				}
-				case JRAlignment.HORIZONTAL_ALIGN_JUSTIFIED :
-				{
-					horizontalAlignment = Element.ALIGN_JUSTIFIED;
-					break;
-				}
-				default :
-				{
+				break;
+			}
+			case JRAlignment.HORIZONTAL_ALIGN_CENTER: {
+				horizontalAlignment = Element.ALIGN_CENTER;
+				break;
+			}
+			case JRAlignment.HORIZONTAL_ALIGN_RIGHT: {
+				if (text.getRunDirection() == JRPrintText.RUN_DIRECTION_LTR) {
+					horizontalAlignment = Element.ALIGN_RIGHT;
+				} else {
 					horizontalAlignment = Element.ALIGN_LEFT;
 				}
+				break;
+			}
+			case JRAlignment.HORIZONTAL_ALIGN_JUSTIFIED: {
+				horizontalAlignment = Element.ALIGN_JUSTIFIED;
+				break;
+			}
+			default: {
+				horizontalAlignment = Element.ALIGN_LEFT;
+			}
 			}
 
 			float verticalOffset = 0f;
-			switch (text.getVerticalAlignment())
-			{
-				case JRAlignment.VERTICAL_ALIGN_TOP :
-				{
-					verticalOffset = 0f;
+			switch (text.getVerticalAlignment()) {
+			case JRAlignment.VERTICAL_ALIGN_TOP: {
+				verticalOffset = 0f;
+				break;
+			}
+			case JRAlignment.VERTICAL_ALIGN_MIDDLE: {
+				verticalOffset = (height - topPadding - bottomPadding - text
+						.getTextHeight()) / 2f;
+				break;
+			}
+			case JRAlignment.VERTICAL_ALIGN_BOTTOM: {
+				verticalOffset = height - topPadding - bottomPadding
+						- text.getTextHeight();
+				break;
+			}
+			default: {
+				verticalOffset = 0f;
+			}
+			}
+
+			float llx = x + leftPadding;
+			float lly = jasperPrint.getPageHeight() - y - topPadding
+					- verticalOffset - text.getLeadingOffset();
+			float urx = x + width - rightPadding;
+			float ury = jasperPrint.getPageHeight() - y - height
+					+ bottomPadding;
+			boolean isOver = false;
+			int status = ColumnText.START_COLUMN;
+			Phrase phrase = getPhrase(styledText, text);
+
+			ColumnText colText = new ColumnText(pdfContentByte);
+			colText.setSimpleColumn(phrase, llx, lly, urx, ury, 0,// text.getLineSpacingFactor(),//
+																	// *
+																	// text.getFont().getSize(),
+					horizontalAlignment);
+
+			colText.setLeading(0, text.getLineSpacingFactor());// *
+																// text.getFont().getSize());
+			colText
+					.setRunDirection(text.getRunDirection() == JRPrintText.RUN_DIRECTION_LTR ? PdfWriter.RUN_DIRECTION_LTR
+							: PdfWriter.RUN_DIRECTION_RTL);
+
+			float yLine = colText.getYLine();
+			
+			// 模拟将ColumnText录入
+			while (colText.hasMoreText(status)) {
+				status = colText.go(true);
+				colText.setYLine(yLine);
+
+				// 如果没有更多的行可以放置文本数据,则跳出循环并设置溢出标记为true
+				if (status == ColumnText.NO_MORE_COLUMN) {
+					isOver = true;
 					break;
-				}
-				case JRAlignment.VERTICAL_ALIGN_MIDDLE :
-				{
-					verticalOffset = (height - topPadding - bottomPadding - text.getTextHeight()) / 2f;
-					break;
-				}
-				case JRAlignment.VERTICAL_ALIGN_BOTTOM :
-				{
-					verticalOffset = height - topPadding - bottomPadding - text.getTextHeight();
-					break;
-				}
-				default :
-				{
-					verticalOffset = 0f;
 				}
 			}
 
-			Rectangle r = new Rectangle(
-					x + leftPadding,
-					jasperPrint.getPageHeight()- y- topPadding- verticalOffset- text.getLeadingOffset(),
-					x + width - rightPadding,
-					jasperPrint.getPageHeight()- y- height+ bottomPadding
-				);
-			String key = text.getKey();//PdfText的唯一id
-			if(alreadyExistFields.contains(key)){ //已经有了
-				key=createUniqueName();
-			}
-			alreadyExistFields.add(key);
-			TextField tf = new TextField(pdfContentByte.getPdfWriter(),r,key);
-			tf.setAlignment(horizontalAlignment);
-			tf.setText(text.getText());
-			tf.setFont(PdfUtils.createSongLightBaseFont());
-			tf.setOptions(TextField.MULTILINE);
-			try {
-				pdfContentByte.getPdfWriter().addAnnotation(tf.getTextField());
-			} catch (IOException e) {
-				e.printStackTrace();
+			// 如果没有溢出,则将ColumnText写入
+			if (!isOver) {
+				colText.setText(phrase);
+				status = ColumnText.START_COLUMN;
+				
+				while (colText.hasMoreText(status)) {
+					status = colText.go();
+					colText.setYLine(yLine);
+				}
+
+			} else {
+				// 溢出了则用TextField,它来负责自动缩小字体
+				String key = text.getKey();// PdfText的唯一id
+				if (alreadyExistFields.contains(key)) { // 已经有了
+					key = createUniqueName();
+				}
+				alreadyExistFields.add(key);
+				TextField tf = new TextField(pdfContentByte.getPdfWriter(),
+						new Rectangle(llx, lly, urx, ury), key);
+				tf.setAlignment(horizontalAlignment);
+				tf.setText(text.getText());
+				tf.setFont(PdfUtils.createSongLightBaseFont());
+				tf.setOptions(TextField.MULTILINE);
+				try {
+					pdfContentByte.getPdfWriter().addAnnotation(
+							tf.getTextField());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 
@@ -288,10 +293,7 @@ public class CornerPdfExporter extends JRPdfExporter {
 		pdfContentByte.transform(atrans);
 
 		/*   */
-		exportBox(
-			text,
-			text
-			);
+		exportBox(text, text);
 	}
 
 }
