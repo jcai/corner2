74f933ee3a327c5930b95ce6de65e6b96d9a7fa5
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-03-12 07:06:13 +0000

    调整淡入淡出js
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@3982 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/resources/corner/window/fade.js
+++ src/test/resources/corner/window/fade.js
@@ -3,8 +3,8 @@ var fade_isIe=(window.ActiveXObject)?true:false;
 var fade_intAlphaStep=(fade_isIe)?5:0.05; 
 var fade_curSObj=null; 
 var fade_curOpacity=null;
-function startObjVisible(objId,action) 
-{ 
+var fade_timer=null;
+function startObjVisible(objId,action) {
 	fade_curSObj=document.getElementById(objId);
 	if(action == "show"){
 		if(fade_curSObj.style.display=="none"){
@@ -16,48 +16,69 @@ function startObjVisible(objId,action)
 		}
 	}
 } 
-function setObjState(evTarget) 
-{ 
-	if (fade_curSObj.style.display==""){fade_curOpacity=1;setObjClose();} 
-	else{ 
-		if(fade_isIe) 
-		{ 
-			fade_curSObj.style.cssText='DISPLAY: none;Z-INDEX: 1; FILTER: alpha(opacity=0); POSITION: absolute;'; 
-			fade_curSObj.filters.alpha.opacity=0; 
-		}else 
-		{ 
+/*
+ * 设置状态
+ */
+function setObjState(evTarget) {
+	if (fade_curSObj.style.display==""){
+		fade_curOpacity=1;
+		setObjClose();
+	}else{
+		if(fade_isIe){
+			fade_curSObj.style.cssText='DISPLAY: none;Z-INDEX: 1; FILTER: alpha(opacity=0); POSITION: absolute;';
+			fade_curSObj.filters.alpha.opacity=0;
+		}else{
 			fade_curSObj.style.opacity=0 
-		} 
-		fade_curSObj.style.display=''; 
-		fade_curOpacity=0; 
-		setObjOpen(); 
-	} 
+		}
+		fade_curSObj.style.display='';
+		fade_curOpacity=0;
+		setObjOpen();
+	}
 }
-
+/*
+ * 打开层
+ */
 function setObjOpen(){
 	if(fade_isIe){
-		fade_curSObj.filters.alpha.opacity+=fade_intAlphaStep; 
-		if (fade_curSObj.filters.alpha.opacity<100) setTimeout('setObjOpen()',fade_intTimeStep); 
+		fade_curSObj.filters.alpha.opacity+=fade_intAlphaStep;
+		if (fade_curSObj.filters.alpha.opacity<100) {
+			fade_timer=setTimeout('setObjOpen()',fade_intTimeStep)
+		};
 	}else{
-		fade_curOpacity+=fade_intAlphaStep; 
-		fade_curSObj.style.opacity = fade_curOpacity; 
-		if (fade_curOpacity<1) setTimeout('setObjOpen()',fade_intTimeStep); 
+		fade_curOpacity+=fade_intAlphaStep;
+		fade_curSObj.style.opacity = fade_curOpacity;
+		if (fade_curOpacity<1) {
+			fade_timer=setTimeout('setObjOpen()',fade_intTimeStep)
+		};
 	}
-} 
- 
-function setObjClose()
-{ 
-	if(fade_isIe) 
-	{ 
-		fade_curSObj.filters.alpha.opacity-=fade_intAlphaStep; 
-		if (fade_curSObj.filters.alpha.opacity>0) { 
-			setTimeout('setObjClose()',fade_intTimeStep);
-		}else {fade_curSObj.style.display="none";} 
+}
+/*
+ * 关闭层
+ */ 
+function setObjClose(){
+	if(fade_isIe){
+		fade_curSObj.filters.alpha.opacity-=fade_intAlphaStep;
+		if (fade_curSObj.filters.alpha.opacity>0) {
+			fade_timer=setTimeout('setObjClose()',fade_intTimeStep);
+		}else {fade_curSObj.style.display="none";}
 	}else{
-		fade_curOpacity-=fade_intAlphaStep; 
-		if (fade_curOpacity>0) { 
-		fade_curSObj.style.opacity =fade_curOpacity; 
-		setTimeout('setObjClose()',fade_intTimeStep);} 
+		fade_curOpacity-=fade_intAlphaStep;
+		if (fade_curOpacity>0) {
+			fade_curSObj.style.opacity =fade_curOpacity;
+			fade_timer=setTimeout('setObjClose()',fade_intTimeStep);
+		}
 		else {fade_curSObj.style.display='none';} 
-	} 
-} 
+	}
+}
+/*
+ * 保持层状态
+ */
+function keepState(){
+	clearTimeout(fade_timer);
+}
+/*
+ * 调用页面方法
+ */
+function callFun(func){
+	eval(func+"()");
+}
