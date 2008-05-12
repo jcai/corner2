0cf117a8d3113f69e89cd9ddb89cb23fd370646c
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-05-12 10:12:57 +0000

    增加跳转tree js
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4022 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/tapestry/component/tree/page/pagetree.js
@@ -0,0 +1,93 @@
+var PageTree = Class.create();
+PageTree.prototype = {
+	initialize: function(element,page,title,actionFrame) {
+		Ajax.Tree.Invoice = Ajax.Tree.create({
+				types: {
+					pageTreeSite: {
+						page: page,
+						insertion: function(el,data){
+							var node = Builder.node('dl',{className:'treeDl'},
+							[
+								Builder.node('dt',{className:'treeDt'},[data.name]),
+							]);
+							el.appendChild(node);
+
+							this.element.setAttribute("left",data.left);
+							this.element.setAttribute("right",data.right);
+							this.element.setAttribute("depth",data.depth);
+							this.element.setAttribute("treeName",data.name);
+							this.element.setAttribute("this",data.thisEntity);
+							
+							this.element.setAttribute("target",actionFrame);
+							
+							//变量json名值对，增加返回内容
+							for(var key in data.returnDates){
+								this.element.setAttribute(key,data.returnDates[key]);
+							}
+
+							if((data.right - data.left) == 1){
+								this.clickExpense = function(evt){
+									window.parent[actionFrame].location.replace(data.actionPage);
+								}.bind(this);
+								Event.observe(this.span,'click',this.clickExpense);
+							}else{
+								this.clickExpense = function(evt){
+									this.onClick();
+								}.bind(this);
+								Event.observe(this.span,'click',this.clickExpense);
+							}
+						},
+						callback:{
+							call:function(node,id){
+								var left=node.element.getAttribute("left");
+								var right=node.element.getAttribute("right");
+								var depth=node.element.getAttribute("depth");
+								
+								var dependFields;
+								
+//								var dependFields = queryBox.options.dependFields;
+								
+								var depend="";
+								
+								if(dependFields != null && dependFields != "" && dependFields.length != 0){
+									for(var i=0;i<dependFields.length;i++){
+										if(i!=0){
+											depend += ",";
+										}
+										depend += parent.document.getElementById(dependFields[i]).value;
+									}
+								}else{
+									depend="";
+								}
+								
+								var queryClassName;
+//								queryClassName = queryBox.options.queryClassName;
+								
+								var page;
+//								page = queryBox.options.page;
+															
+								return "left=" + left + "&" + "right=" + right + "&" + "depth=" + depth + 
+								"&" + "queryClassName=" + queryClassName + "&" + "dependFields=" + depend + 
+								"&" + "parentPage=" + page;
+							}
+						}
+					}
+				},
+				createNodes: function(nodes){
+					if(!nodes.length){ return; }
+					this.showChildren();
+					this.loaded = true;
+					for(var i=0; i < nodes.length; i++){
+						if((nodes[i].data.right - nodes[i].data.left) == 1){
+							this.options.leafNode = true;
+						}else{
+							this.options.leafNode = false;
+						}
+						var newNode = new this.constructor(this.element,nodes[i].id,nodes[i].type,nodes[i]);
+					}
+					if(this.options.sortable){ this.createSortable(); }
+				}
+			});
+			this.treeObj = new Ajax.Tree.Invoice(element,'root','pageTreeSite',{data:{name:title,left:-1,right:-1,depth:0}});
+	}
+}
