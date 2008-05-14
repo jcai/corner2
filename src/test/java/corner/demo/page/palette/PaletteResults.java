3c8175a0e1ebbc83acb52748b44f9491fdf34ca7
Ghostbb <Ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-05-14 01:13:51 +0000

    增加了确是的图片，修正了返回跳转的错误
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4028 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/test/java/corner/demo/page/palette/PaletteResults.java
+++ src/test/java/corner/demo/page/palette/PaletteResults.java
@@ -1,9 +1,18 @@
 package corner.demo.page.palette;
 
 import java.util.List;
+
+import org.apache.tapestry.IAsset;
+import org.apache.tapestry.annotations.Asset;
 import org.apache.tapestry.html.BasePage;
 
 public abstract class PaletteResults extends BasePage
 {
     public abstract void setSelectedColors(List list);
+    
+    @Asset("classpath:Back.gif")
+    public abstract IAsset getBackImage();
+    
+    @Asset("classpath:Back-focus.gif")
+    public abstract IAsset getBackFocusImage();
 }
