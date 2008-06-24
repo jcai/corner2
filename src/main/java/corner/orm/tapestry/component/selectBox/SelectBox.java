0efa3a4bfb255d4af15afcbc79d59bd1e3b47550
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-06-24 05:07:13 +0000

    corner使用h2
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4107 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/main/java/corner/orm/tapestry/component/selectBox/SelectBox.java
@@ -0,0 +1,25 @@
+//==============================================================================
+// file :       $Id$
+// project:     corner2.5
+//
+// last change: date:       $Date$
+//              by:         $Author$
+//              revision:   $Revision$
+//------------------------------------------------------------------------------
+//copyright:	Beijing Maxinfo Technology Ltd. http://www.bjmaxinfo.com
+//License:      the Apache License, Version 2.0 (the "License")
+//==============================================================================
+
+package corner.orm.tapestry.component.selectBox;
+
+import org.apache.tapestry.BaseComponent;
+
+/**
+ * 两个select互选选择
+ * @author <a href="mailto:xf@bjmaxinfo.com">xiafei</a>
+ * @version $Revision$
+ * @since 2.5
+ */
+public abstract class SelectBox extends BaseComponent{
+	
+}
