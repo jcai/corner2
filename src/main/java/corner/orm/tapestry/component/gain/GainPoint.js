2c8e46f4a0c330f44464cb172cf9947656709947
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-06-26 04:02:25 +0000

    重构gp的js
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4123 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/gain/GainPoint.js
+++ src/main/java/corner/orm/tapestry/component/gain/GainPoint.js
@@ -2,63 +2,106 @@
 	Copyright (c) Beijing Maxinfo Technology Co.,Ltd.
 	All Rights Reserved.
 */
-function addRows(size,id){
+var GainPoint = Class.create();
+GainPoint.prototype = {
+	initialize: function(tableId,pagePersistentId,size,flags,checkBoxs) {
+		this.tableId = tableId;
+		this.pagePersistentId = pagePersistentId;
+		this.size = size;
+		this.flags = flags;
+		this.checkBoxs = checkBoxs;
+		
+		//初始化
+		this.addRows();
+		this.init();
+	},
+	init:function(){
+		var temp;
+		for(var key in this.flags){
+			var elements = document.getElementsByName(key);
+			dojo.debug("elements.length " + elements.length);
+			
+			for (var i in elements){
+				temp = 	this.flags[key][i];
+				dojo.debug("type " + elements[i].type);
+				
+				if(temp){
+					elements[i].value = temp;
+					if(isContain(this.checkBoxs,key)){
+						if(temp=="on"){
+							document.getElementsByName(key+"_check")[i].checked = "checked";
+						}
+					}
+				}
+			}
+		}
+	},
+	addRows:function(){
+	//summary:
+	// 传入循环次数和tableID，批量增加
+		for(var i=0 ; i < this.size-1;i++){
+			this.addRow();
+		}
+	},
+	addRow : function(){
+	//summary:
+	// 传入ID，获得要增加行的table
+		var tblObj = dojo.byId(this.tableId);
+		if(tblObj.rows){
+			dojo.debug(tblObj.rows.length);
+		}else{
+			dojo.debug('aaa');
+		}
+		//追加行
+		var newRow = tblObj.insertRow(-1);
+		newRow.style.display = "";
+		var cellNum = tblObj.rows[0].cells.length;
+		
+		//追加列
+		for(colIndex = 0; colIndex < cellNum; colIndex++){
+			var newCell = newRow.insertCell(-1);
+			initTblCell(newCell,this.tableId);
+		}
+		
+		//删除最后一个增加的id
+		var values = document.getElementsByName(this.pagePersistentId);
+		
+		if(values.length - 1 <= 0){ //如果是0
+			return;
+		}
+		
+		var field = values[values.length - 1];
+		
+		field.value="";
+	},
+	delLastRow : function(){
+	//summary:
+	// 删除指定table的最后一个tr
+		var obj = dojo.byId(this.tableId);
+		var id = obj.rows.length-1;
+		delRowById(this.tableId,id);
+	},
+	delRowById : function(obj){
+		delRowById(this.tableId,obj.parentNode.parentNode.rowIndex)
+	}
+}
+
+var isContain = function (checkBoxs,value){
 //summary:
-// 传入循环次数和tableID，批量增加
-	for(var i=0 ; i < size-1;i++){
-		addRow(id);
+// 是否是checkbox
+	for(var i = 0 ; i<checkBoxs.length;i++){
+		if(value==checkBoxs[i]){
+			return true;
+		}
 	}
+	return false;
 }
 
-function getTrIndex(obj){
-	return obj.parentNode.parentNode.rowIndex;
-}
-
-function addRow(id){
-//summary:
-// 传入ID，获得要增加行的table
-	var tblObj = dojo.byId(id);
-	if(tblObj.rows){
-		dojo.debug(tblObj.rows.length);
-	}else{
-		dojo.debug('aaa');
-	}
-	//追加行
-	var newRow = tblObj.insertRow(-1);
-	newRow.style.display = "";
-	var cellNum = tblObj.rows[0].cells.length;
-	
-	//追加列
-	for(colIndex = 0; colIndex < cellNum; colIndex++){
-		var newCell = newRow.insertCell(-1);
-		initTblCell(newCell,id);
-	}
-	
-	//删除最后一个增加的id
-	var values = document.getElementsByName(getPagePersistentId());
-	
-	if(values.length - 1 <= 0){ //如果是0
-		return;
-	}
-	
-	var field = values[values.length - 1];
-	
-	field.value="";
-}
-
-function getPagePersistentId(){
-//summary:
-// 获得持久化ID
-	return 'poid';
-}
-
-function initTblCell(cell,id){
+var initTblCell = function(cell,id){
 //summary:
 // 传入复制的样子和ID，增加新的一行
 	var lastCell = dojo.byId(id).rows[0].cells[cell.cellIndex];
 	cell.innerHTML = lastCell.innerHTML;
-	
-//	dojo.debug(cell.innerHTML);
 	
 	if(cell.children != null && cell.children.length > 0){
 		for(childIndex = 0;childIndex < cell.children.length; childIndex++){
@@ -81,7 +124,7 @@ function initTblCell(cell,id){
 	cell.height = lastCell.height;
 }
 
-function dellRowById(pid,id){
+var delRowById = function(pid,id){
 //summary:
 // 删除指定table的指定行
 	var obj = dojo.byId(pid);
@@ -116,16 +159,9 @@ function dellRowById(pid,id){
 	}
 }
 
-function dellLastRow(pid){
+var checkHidField = function(hidName,filed){
 //summary:
-// 删除指定table的最后一个tr
-	var obj = dojo.byId(pid);
-	var id = obj.rows.length-1;
-	dellRowById(pid,id);
-}
-
-
-function checkHidField(hidName,filed){
+//对hidden进行处理
 	var obj = document.getElementsByName(hidName+"_check");
 	//获得选择的位置
 	var index = 0;
