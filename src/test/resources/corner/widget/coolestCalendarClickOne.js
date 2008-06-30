ec770c27d66a9045ee2ce4b8788453eac13a9c5a
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-06-30 07:37:21 +0000

    增加coolest js测试
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4139 bda6c913-8cf9-0310-89cc-b5856a464ced

---
+++ src/test/resources/corner/widget/coolestCalendarClickOne.js
@@ -0,0 +1,49 @@
+// This function gets called when the end-user clicks on some date.
+function selected(cal, date) {
+  cal.sel.value = date;
+  //cal.callCloseHandler();
+}
+
+// And this gets called when the end-user clicks on the _selected_ date,
+// or clicks on the "Close" button.  It just hides the calendar without
+// destroying it.
+function closeHandler(cal) {
+  cal.hide();
+  _dynarch_popupCalendar = null;
+}
+
+// This function shows the calendar under the element having the given id.
+// It takes care of catching "mousedown" signals on document and hiding the
+// calendar if the click was outside.
+function showCalendar(id, format, showsTime, showsOtherMonths) {
+  var el = document.getElementById(id);
+  if (_dynarch_popupCalendar != null) {
+    // we already have some calendar created
+    _dynarch_popupCalendar.hide();                 // so we hide it first.
+  } else {
+    // first-time call, create the calendar.
+    var cal = new Calendar(1, null, selected, closeHandler);
+    // uncomment the following line to hide the week numbers
+    // cal.weekNumbers = false;
+    if (typeof showsTime == "string") {
+      cal.showsTime = true;
+      cal.time24 = (showsTime == "24");
+    }
+    if (showsOtherMonths) {
+      cal.showsOtherMonths = true;
+    }
+    _dynarch_popupCalendar = cal;                  // remember it in the global var
+    cal.setRange(1900, 2070);        // min/max year allowed.
+    cal.create();
+  }
+  _dynarch_popupCalendar.setDateFormat(format);    // set the specified date format
+  _dynarch_popupCalendar.parseDate(el.value);      // try to parse the text in field
+  _dynarch_popupCalendar.sel = el;                 // inform it what input field we use
+
+  // the reference element that we pass to showAtElement is the button that
+  // triggers the calendar.  In this example we align the calendar bottom-right
+  // to the button.
+  _dynarch_popupCalendar.showAtElement(el.nextSibling, "Br");        // show the calendar
+
+  return false;
+}
