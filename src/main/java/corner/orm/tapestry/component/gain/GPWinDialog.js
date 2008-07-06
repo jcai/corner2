0f05d285aa1ab6c14dd78de3bd77425e203156e0
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-07-06 05:59:04 +0000

    增加gp弹出选择树
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4158 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/gain/GPWinDialog.js
+++ src/main/java/corner/orm/tapestry/component/gain/GPWinDialog.js
@@ -7,3 +7,15 @@ function initWindowDialog(element, title, url,selectFunName) {
 								   "opacity":1,"recenterAuto":"true","gridX":1,"gridY":1,"wiredDrag":"false",
 								   "destroyOnClose":"false","all callbacks":"none"},selectFunName);
 }
+
+//弹出树
+function initWindowDialog(element, title, url,selectFunName,treeName,queryClassName,dependFields,page) {
+	new LeftTreeDialog(element,{"className":"alphacube","title":title,
+									   "url":url,"top ":"top:0","right":"left:0","width":"500","height":"300",
+									   "maxWidth":"none","maxHeight":"none","minWidth ":"500","minHeight":"300",
+									   "resizable":"true","closable":"true","minimizable":"true","maximizable":"true",
+									   "draggable":"true","showEffectOptions":"none","hideEffectOptions":"none","effectOptions":"none",
+									   "opacity":1,"recenterAuto":"true","gridX":1,"gridY":1,"wiredDrag":"false",
+									   "destroyOnClose":"false","all callbacks":"none"},selectFunName,
+									   treeName,queryClassName,dependFields,page);
+}
