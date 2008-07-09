d6593b5381a3a777aaa6ebdcea9349e7173f3262
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-07-09 08:10:59 +0000

    将ajax写成测试
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4170 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/response/ajaxResponse.js
+++ src/main/java/corner/orm/tapestry/component/response/ajaxResponse.js
@@ -1,12 +1,26 @@
-var ajaxResponse = function(url,paras){  
-    var response = "";
-    request = new Ajax.Request(url,{
-		parameters: paras,
-	    method: 'get',
-	    onSuccess: function(transport){
-    		response = transport.responseText; 
-    	},
-    	asynchronous:false
-	 });
-     return response;
-}
+/*
+ * 进行ajax请求到任意页面，返回json串
+ */
+AjaxResponse = CornerBuilder.create({
+	extend: CornerBase.prototype,
+	initialize: function(url,page) {
+		this.url = url;
+		this.page = page;
+	},
+	request:function(paras){
+		dojo.debug(paras);
+		if(paras == null) paras = {};
+		var response = "";
+		var ops = {"page":this.page};
+		Object.extend(paras, ops);
+	    request = new Ajax.Request(this.url,{
+			parameters: paras,
+		    method: 'get',
+		    onSuccess: function(transport){
+	    		response = transport.responseText; 
+	    	},
+	    	asynchronous:false
+		 });
+	     return response;
+	}
+});
