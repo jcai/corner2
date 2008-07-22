38f4d49a7f128099cb0c3f164a730c1087255a13
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-07-22 06:37:53 +0000

    调整codeac默认显示中文
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4207 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/prototype/autocompleter/CodeModelAutocompleter.java
+++ src/main/java/corner/orm/tapestry/component/prototype/autocompleter/CodeModelAutocompleter.java
@@ -29,24 +29,27 @@ import org.apache.tapestry.annotations.Parameter;
  * @since 2.3.7
  */
 public abstract class CodeModelAutocompleter extends BaseAutocompleter {
-	 
 
-
-    
 	/** 选择的过滤器 **/
 	@Parameter(defaultValue="ognl:new corner.orm.tapestry.component.prototype.autocompleter.CodeSelectModel()")
 	public abstract ISelectModel getSelectModel();
 	
 	/**
 	 * 获得样式模板
+	 * @see corner.orm.tapestry.component.prototype.autocompleter.BaseAutocompleter#getTemplate()
 	 */
-	@Parameter(defaultValue = "literal:<span class=\"selectme\">%1$s</span>/%3$s/%2$s")
+	@Parameter(defaultValue = "literal:%1$s/<span class=\"selectme\">%3$s</span>/%2$s")
 	public abstract String getTemplate();
 	
+	/**
+	 * @see corner.orm.tapestry.component.prototype.autocompleter.BaseAutocompleter#getOptions()
+	 */
 	@Parameter(defaultValue="literal:{select:'selectme'}")
 	public abstract String getOptions();
 	
-
+	/**
+	 * @see corner.orm.tapestry.component.prototype.autocompleter.BaseAutocompleter#constructSelectModel()
+	 */
 	@Override
 	protected ISelectModel constructSelectModel() {
 		return this.getSelectModel();
