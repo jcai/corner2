9a04e8e881054aca8b3259b68533c064b7591542
ghostbb <ghostbb@bda6c913-8cf9-0310-89cc-b5856a464ced>
2008-10-13 06:41:22 +0000

    修正了timeTranslator的错误
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4295 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/translator/TimeTranslator.java
+++ src/main/java/corner/orm/tapestry/translator/TimeTranslator.java
@@ -23,7 +23,7 @@ import java.text.SimpleDateFormat;
 /**
  * 一个时间类型的Translator
  * <p>
- * 将当前时间格式化成HH:MM:ss的格式的translator
+ * 将当前时间格式化成HH:mm:ss的格式的translator
  * 
  * @author <a href="mailto:Ghostbb@bjmaxinfo.com">Ghostbb</a>
  * @version $Revision$
@@ -33,7 +33,7 @@ public class TimeTranslator extends org.apache.tapestry.form.translator.DateTran
     /**
      * corner中时间类型使用的pattern
      */
-    private static final String CORNER_TIME_PATTERN = "HH:MM:ss";
+    private static final String CORNER_TIME_PATTERN = "HH:mm:ss";
     
     /**
      * 用户回显时使用的Format
