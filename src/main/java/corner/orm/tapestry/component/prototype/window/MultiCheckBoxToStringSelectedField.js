f3f305a8023f81843b39c654d93b95b250ab7eb7
lsq <lsq@bda6c913-8cf9-0310-89cc-b5856a464ced>
2009-02-03 09:36:10 +0000

    使用全局变量的方式 进行回显
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4356 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/prototype/window/MultiCheckBoxToStringSelectedField.js
+++ src/main/java/corner/orm/tapestry/component/prototype/window/MultiCheckBoxToStringSelectedField.js
@@ -2,32 +2,48 @@ var checkboxEventConnect = function(id,box){
 	dojo.event.connect(dojo.byId(id),"onclick",function(evt){
 		var src=dojo.html.getEventTarget(evt);
 		var queryBox = eval(box);
+		dojo.debug("B queryBox.maps.values is "+queryBox.maps.values());
 		if(src.checked){
 			queryBox.maps[src.value]=src.value;
 		}else{
 			delete queryBox.maps[src.value];
 		}
+		dojo.debug("queryBox.maps.values is "+queryBox.maps.values());
 		queryBox.onSelect(queryBox.maps.values());
 	}); 
 }
+
+ var queryBoxStr,intervalId;//TODO 全局变量可能造成问题,期待解决.
 /*
  * 变成选择状态
  */
 var loadCheckBoxSelected = function(box){
-	dojo.event.connect('after',window, 'onload',function(evt){
+   queryBoxStr = box;
+   dojo.event.connect('after',window, 'onload',"loadCheckBox");
+}
+
+var loadCheckBox = function(evt){
+		var queryBox;
+				
+		try{
+		    queryBox = eval(queryBoxStr);
+		    queryBox.maps;
+		}catch(e){
+		    intervalId = window.setInterval("loadCheckBox()",1000);
+			dojo.debug("interval "+intervalId);
+			return;
+		}
+				
+		window.clearInterval(intervalId);
+				
 		var elements = [];
 		var inputs = document.getElementsByTagName("input");
 		dojo.lang.forEach(inputs, function(input){
-			if(input.getAttribute("type")=="checkbox"){
-				elements.push(input);
-			}
-		});
-		var queryBox = eval(box);
-		dojo.debug(queryBox);
-		if(queryBox == undefined || queryBox == null){
-			window.setInterval("loadCheckBoxSelected()",1000);
-			return;
+		if(input.getAttribute("type")=="checkbox"){
+			elements.push(input);
 		}
+		});
+				
 		for(var key in queryBox.maps){
 			for(var i=0;i<elements.length;i++){
 				if(elements[i].value==key){
@@ -35,5 +51,4 @@ var loadCheckBoxSelected = function(box){
 				}
 			}
 		}
-	});
-}
+	    }
