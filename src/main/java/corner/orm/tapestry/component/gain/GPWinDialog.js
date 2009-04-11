5b0a9973ecd117563f1528b35b17de72a66189c7
xf <xf@bda6c913-8cf9-0310-89cc-b5856a464ced>
2009-04-11 07:33:44 +0000

    修改弹出错误
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4414 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/gain/GPWinDialog.js
+++ src/main/java/corner/orm/tapestry/component/gain/GPWinDialog.js
@@ -11,7 +11,7 @@ function initWindowDialog(element, title, url,selectFunName) {
 //弹出树
 function initWindowDialog(element, title, url,selectFunName,treeName,queryClassName,dependFields,page) {
 	new LeftTreeDialog(element,{"className":"alphacube","title":title,
-									   "url":url,"top ":"top:0","right":"left:0","width":"500","height":"300",
+									   "url":url,"top ":"top:0","right":"0","width":"500","height":"300",
 									   "maxWidth":"none","maxHeight":"none","minWidth ":"500","minHeight":"300",
 									   "resizable":"true","closable":"true","minimizable":"true","maximizable":"true",
 									   "draggable":"true","showEffectOptions":"none","hideEffectOptions":"none","effectOptions":"none",
