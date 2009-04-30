97dd6a8db79f72f95232483a8567a0cfa714f3af
wlh <wlh@bda6c913-8cf9-0310-89cc-b5856a464ced>
2009-04-30 06:27:22 +0000

    tafelTree修改。
    git-svn-id: http://dev.bjmaxinfo.com/svn/corner/trunk/corner@4429 bda6c913-8cf9-0310-89cc-b5856a464ced

--- src/main/java/corner/orm/tapestry/component/tree/tafeltree/TafelTreeUtil.js
+++ src/main/java/corner/orm/tapestry/component/tree/tafeltree/TafelTreeUtil.js
@@ -1,6 +1,44 @@
-//tafelTree关闭节点
-function tafelTreeExpend(branches){
-		for(var i=0;i<branches.length;i++){
-			branches[i].collapse();
+var CustTafelTree = Class.create();
+CustTafelTree.prototype = {
+	initialize: function(struct,treeConfig,treeId,rootId,expendElementId,collapseElementId){
+
+		var tree = null;
+		// This structure represents one root node with two children
+//		var struct = ${treeStruct};
+//		var treeConfig = ${treeConfig};
+		
+		// Tree initialisation. This function is called automatically
+		// when the page finish to load
+		tree = new TafelTree(treeId, struct, treeConfig);
+//		tree.generate(); 
+		
+		expend = function(){
+			tree.expend();
 		}
-};
+		collapseTree = function(){
+			tree.collapse();
+		}
+		//tafelTree关闭节点
+		tafelTreeExpend= function(branches){
+			for(var i=0;i<branches.length;i++){
+				branches[i].collapse();
+			}
+		};
+		
+		var root = tree.getBranchById(rootId);
+		root.expend();
+		var branches = root.getBranches();
+		
+		tafelTreeExpend(branches);
+
+		if(expendElementId!=''){
+			dojo.event.connect(dojo.byId(expendElementId),'onclick','expend');
+		}
+		
+		if(collapseElementId!=''){
+			dojo.event.connect(dojo.byId(collapseElementId),'onclick','collapseTree');
+		}
+	}
+}
+
+
